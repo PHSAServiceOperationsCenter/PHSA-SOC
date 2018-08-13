7e4b641e6b72abdeaee6d0df2ffa51f07114f5a2
serban teodorescu <serbant@gmail.com>
2018-08-13 08:14:22 -0700

    add method update_or_create_from_orion
    class method that runs model specific Orion queries and uses the results
    to populate said models

--- orion_integration/models.py
+++ orion_integration/models.py
@@ -14,47 +14,78 @@ django models for the orion_integration app
 """
 __updated__ = '2018_08_08'
 
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.db import IntegrityError, models
+from django.utils.translation import gettext_lazy as _
 
 from p_soc_auto_base.models import BaseModel
+
+from .orion import OrionClient
 
 
 class OrionBaseModel(BaseModel, models.Model):
     """
     most (if not all) Orion objects need an orion_id key that comes from the
     Orion server
-    
+
     then we also need the fields from the BaseModel; let's use this as the
     base class for all the models here
     """
     orion_id = models.BigIntegerField(
-        _('Orion Object Id'), db_index=True, unique=True, blank=False, 
+        _('Orion Object Id'), db_index=True, unique=True, blank=False,
         help_text=_(
             'Use the value in this field to query the Orion server'))
-    
+
+    @classmethod
+    def update_or_create_from_orion(cls):
+        """
+        class method to update or create orion model instances
+
+        update or create entries in orion models using data from the Orion
+        server
+
+        :arg cls: the model class
+
+            note that this method is defined in a base class so that it can
+            be invoked from all the classes inheriting form that particular
+            base class
+        """
+        user = cls.get_or_create_user(settings.ORION_USER)
+        data = OrionClient.populate_from_query(cls)
+        for data_item in data:
+            orion_mapping = dict()
+            for mapping in cls.orion_mappings:
+                orion_mapping[mapping[0]] = data_item[mapping[1]]
+
+            orion_mapping.update(update_by=user)
+            try:
+                cls.objects.update_or_create(**orion_mapping)
+            except IntegrityError:
+                orion_mapping.update(created_by=user)
+                cls.objects.update_or_create(**orion_mapping)
+
     class Meta:
-        abstract=True    
-        
+        abstract = True
+
 
 class OrionNodeStatus(OrionBaseModel, models.Model):
     """
     """
     status = models.CharField(
-        _('Node Status'), max_length=254, db_index=True, unique=True, 
+        _('Node Status'), max_length=254, db_index=True, unique=True,
         blank=False, null=False)
-    
-    class Meta:
-        app_label='orion_integration'
-        verbose_name='Orion Node Status'
-        verbose_name_plural='Orion Node Statuses'
-    
 
-class OrionNode(OrionBaseModel,models.Model):
+    class Meta:
+        app_label = 'orion_integration'
+        verbose_name = 'Orion Node Status'
+        verbose_name_plural = 'Orion Node Statuses'
+
+
+class OrionNode(OrionBaseModel, models.Model):
     """
     reference:
     `Orion Node<http://solarwinds.github.io/OrionSDK/schema/Orion.Nodes.html>`_
-    
+
     query:
     SELECT IPAddress, Caption, NodeDescription, Description, DNS, 
     Category, Vendor, Location, Status, MachineType,  NodeName, DetailsUrl
@@ -69,35 +100,38 @@ class OrionNode(OrionBaseModel,models.Model):
     category = models.ForeignKey(
         'OrionNodeCategory', on_delete=models.PROTECT,
         verbose_name=_('Orion Node Category'))
-    ip_address=models.GenericIPAddressField(
+    ip_address = models.GenericIPAddressField(
         _('IP Address'), db_index=True, unique=True, blank=False, null=False,
         protocol='IPv4')
     node_name = models.CharField(
         _('Node Name'), db_index=True, blank=False, null=False, max_length=254)
     node_dns = models.CharField(
         _('DNS'), db_index=True, blank=False, null=False, max_length=254)
-    node_description=models.TextField(_('Orion Node Description'))
+    node_description = models.TextField(_('Orion Node Description'))
     vendor = models.CharField(
         _('Vendor'), db_index=True, blank=True, null=True, max_length=254)
     location = models.CharField(
         _('Location'), db_index=True, blank=True, null=True, max_length=254)
     machine_type = models.CharField(
-        _('Machine Type'), db_index=True, blank=True, null=True, 
+        _('Machine Type'), db_index=True, blank=True, null=True,
         max_length=254, help_text=_('this needs to become a foreign key'))
-    status=models.ForeignKey('OrionNodeStatus', on_delete=models.PROTECT)
-    
+    status = models.ForeignKey('OrionNodeStatus', on_delete=models.PROTECT)
 
-class OrionNodeCategory(OrionBaseModel,models.Model):
+
+class OrionNodeCategory(OrionBaseModel, models.Model):
     """
     query:
     SELECT Description FROM Orion.NodeCategories
     """
+    orion_query = 'SELECT CategoryID, Description FROM Orion.NodeCategories'
+    orion_mappings = (('orion_id', 'CategoryID'),
+                      ('category', 'Description'))
     category = models.CharField(
         _('Orion Node Category'), db_index=True, unique=True, null=False,
         blank=False, max_length=254, help_text=_(
             'Orion Node Category Help'))
-    
+
     class Meta:
         app_label = 'orion_integration'
-        verbose_name= _('Orion Node Category')
+        verbose_name = _('Orion Node Category')
         verbose_name_plural = _('Orion Node Categories')

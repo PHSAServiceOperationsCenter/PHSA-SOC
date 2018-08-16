6156db254c0e70fcd2d803cb685d24bafbbd2269
serban teodorescu <serbant@gmail.com>
2018-08-16 10:55:13 -0700

    remove useless comments

--- p_soc_auto/celery.py
+++ p_soc_auto/celery.py
@@ -10,10 +10,3 @@ app = Celery('p_soc_auto')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-
-#=========================================================================
-# @app.task(bind=True)
-# def debug_task(self):
-#     print('Request: {0!r}'.format(self.request))
-#=========================================================================

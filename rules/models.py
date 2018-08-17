af91b061521b31c002b45b3c1b6eb1bc504cd504
Ali Rahmat- CST HUB developer <arahmat@lvmsocdev01.healthbc.org>
2018-08-17 07:36:07 -0700

    initial demo codes....

---
+++ rules/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.forms import ModelForm
+from django.urls import reverse
+from datetime import date
+from django.utils import timezone
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class MyCertsRule(models.Model):
+    recid = models.AutoField(primary_key=True)
+    intervals= models.CharField(max_length=100, blank=True, null=True)
+    code = models.CharField(max_length=100, blank=True, null=True)
+

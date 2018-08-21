0e9a04f777e256e84795ae3d12546cc2938ba75c
serban teodorescu <serbant@gmail.com>
2018-08-21 15:39:32 -0700

    add module docstring and __version__ global var

--- p_soc_auto/__init__.py
+++ p_soc_auto/__init__.py
@@ -1,3 +1,19 @@
+"""
+.. _p_soc_auto:
+
+django project p_soc_auto
+
+:module:    p_soc_auto
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+"""
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
+__version__ = '0.0.1'

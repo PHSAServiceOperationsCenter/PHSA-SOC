99ec6be535c8000e21205ba819c0a25d73c86e52
PHSAOperationCenter <ali.rahmat@phsa.ca>
2018-08-21 07:49:36 -0700

    cleanup the code

--- pocApp/apps.py
+++ pocApp/apps.py
@@ -1,3 +1,12 @@
+"""
+.. _apps:
+django apps module for NMap app
+:copyright:
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+:contact:    ali.rahmat@phsa.ca
+"""
+__updated__ = '2018_08_21'
 from django.apps import AppConfig
 
 class PocappConfig(AppConfig):

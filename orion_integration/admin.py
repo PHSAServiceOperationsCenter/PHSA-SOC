382f6d0234b7517d58ba3657964e8883de625ca0
serban teodorescu <serbant@gmail.com>
2018-08-30 15:13:47 -0700

    mostly docstrings

--- orion_integration/admin.py
+++ orion_integration/admin.py
@@ -16,22 +16,38 @@ __updated__ = '2018_08_08'
 
 from django.contrib import admin
 
-from .models import OrionNode, OrionNodeCategory, OrionAPMApplication
+from .models import (
+    OrionNode, OrionNodeCategory, OrionAPMApplication, OrionCSTNode,
+)
+
+
+@admin.register(OrionCSTNode)
+class OrionCSTNodeAdmin(admin.ModelAdmin):
+    """
+    admin intrface for the OrionCSTNode model
+    """
+    pass
 
 
 @admin.register(OrionNode)
 class OrionNodeAdmin(admin.ModelAdmin):
+    """
+    admin interface for the OrionNode model
+    """
     pass
 
 
 @admin.register(OrionNodeCategory)
 class OrionNodeCategoryAdmin(admin.ModelAdmin):
+    """
+    admin interface for the OrionNodeCategory model
+    """
     pass
 
 
 @admin.register(OrionAPMApplication)
 class OrionAPMApplicationAdmin(admin.ModelAdmin):
+    """
+    admin interface for the OrionAPApplication model
+    """
     pass
-
-
-# Register your models here.

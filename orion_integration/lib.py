c78f596ff0934cad1a3af99bef391a70915ac2fc
serbant <serbant@gmail.com>
2018-09-05 17:25:21 -0700

    we can now configure whether we accept Orion's definition of SSL nodes or not

--- orion_integration/lib.py
+++ orion_integration/lib.py
@@ -24,29 +24,40 @@ class OrionSslNode():
     ssl_filters = dict(orionapmapplication__application_name__icontains='ssl')
 
     @classmethod
-    def nodes(cls, cerner_cst=True):
+    def nodes(cls, cerner_cst=True, orion_ssl=False):
         """
-        :returns: a django queryset object with all the SSL nodes
-        """
-        if cerner_cst:
-            return OrionCernerCSTNode.objects.filter(**cls.ssl_filters).all()
+        :arg bool cerner_cst: use the Cerner-CST data; default: `True`
 
-        return OrionNode.objects.filter(**cls.ssl_filters).all()
+        :arg bool orion_ssl: accept orion's definition of what an SSL node is
+
+            default: `False`
+
+        :returns: a django queryset of orion node instances
+        """
+        queryset = OrionNode.objects.all()
+
+        if cerner_cst:
+            queryset = OrionCernerCSTNode.objects.all()
+
+        if orion_ssl:
+            return queryset.filter(**cls.ssl_filters).all()
+
+        return queryset
 
     @classmethod
-    def count(cls, cerner_cst=True):
+    def count(cls, cerner_cst=True, orion_ssl=False):
         """
         :returns: the number of SSL nodes
         :rtype: int
         """
-        return cls.nodes(cerner_cst=cerner_cst).count()
+        return cls.nodes(cerner_cst=cerner_cst, orion_ssl=orion_ssl).count()
 
     @classmethod
-    def ip_addresses(cls, cerner_cst=True):
+    def ip_addresses(cls, cerner_cst=True, orion_ssl=False):
         """
         :returns: the list of ip addresses for orion ssl nodes
         :rtype: list
         """
         return list(
-            cls.nodes(cerner_cst=cerner_cst).values_list('ip_address',
-                                                         flat=True))
+            cls.nodes(cerner_cst=cerner_cst, orion_ssl=orion_ssl).
+            values_list('ip_address', flat=True))

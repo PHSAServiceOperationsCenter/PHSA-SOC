eeb376246362460687ddd47073619106943413f4
PHSAOperationCenter <ali.rahmat@phsa.ca>
2018-09-10 15:27:27 -0700

    More unit tests plus using django time utils

--- ssl_cert_tracker/admin.py
+++ ssl_cert_tracker/admin.py
@@ -1,1 +1,34 @@
-# Register your models here
+"""
+.. _admin:
+
+django admin for the orion_integration app
+
+:module:    p_soc_auto.ssl_cert_tracker.admin
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    ali.rahmat@phsa.ca
+"""
+__updated__ = '2018_08_08'
+
+from django.contrib import admin
+
+from .models import NmapCertsData, NmapHistory
+
+
+@admin.register(NmapCertsData)
+class NmapCertsDataAdmin(admin.ModelAdmin):
+    """NmapCertsDataAdmin to be displayed on admin page  """
+    pass
+
+
+@admin.register(NmapHistory)
+class NmapHistoryAdmin(admin.ModelAdmin):
+    """NmapHistoryAdmin to be displayed on admin page  """
+    pass
+
+
+# Register your models here.

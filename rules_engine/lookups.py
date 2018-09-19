d674740f0246417085f9d62c02f07628da4f9a3c
serbant <serbant@gmail.com>
2018-09-19 15:51:56 -0700

    not using lookups anymore, removed

--- rules_engine/lookups.py
+++
@@ -1,9 +0,0 @@
- ct=ContentType.objects.get(model='tindataforruledemos')
- 
- ct.get_all_objects_for_this_type().first()
- 
- ct.get_all_objects_for_this_type().first()._meta.get_fields()
-
-
-[field.name for field in ct.get_all_objects_for_this_type().first(
-)._meta.get_fields() if not field.primary_key and not field.is_relation]

e4ff8b09eb74da67834b837ccd3224618b6116ac
PHSAOperationCenter <ali.rahmat@phsa.ca>
2018-09-21 09:40:24 -0700

    added more fields to noti... model...

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,5 +1,35 @@
+
+"""
+.. _apps:
+
+django apps module for the orion integration app
+
+:module:    p_soc_auto.notifications.apps
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    ali.rahmat@phsa.ca
+
+:updated:    Sep. 20, 2018
+"""
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class NotificationsConfig(AppConfig):
+    """
+    App class for the notifications django app
+    """
     name = 'notifications'
+    verbose_name = _(
+        'PHSA Service notifications Application')
+
+    def ready(self):
+        """
+        in case of circular imports or app not ready place imports
+        in this method
+        """
+        pass

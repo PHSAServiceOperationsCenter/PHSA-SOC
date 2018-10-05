bf8efeff36918e08916689ebbd08252ab0d7bdea
serbant <serbant@gmail.com>
2018-10-05 12:48:47 -0700

    actually enable pytest and run it from the top directory

--- ssl_cert_tracker/tests/test_db_helper.py
+++ ssl_cert_tracker/tests/test_db_helper.py
@@ -14,28 +14,34 @@ django models for the ssl_certificates app
 
 """
 import pytest
+import os
+import sys
+
 from django.utils import timezone
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+
 from ssl_cert_tracker.models import NmapCertsData, NmapHistory
+
 
 @pytest.mark.django_db
 def test_insert_into_certs_data():
     """test_insert_into_certs_data inserts data into model then
     do a sanity check"""
-    NmapCertsData.created_by = NmapCertsData.get_or_create_user(username='PHSA_User')
-    cert = NmapCertsData(xml_data = "sample data", \
-                         addresses = "10.10.10.10", \
-                         orion_id = "12345", \
-                         not_before = timezone.now(), \
-                         not_after = timezone.now(), \
-                         md5 = "57adc7886d93a03d934ae691178748b7", \
-                         sha1 = "ae699d5ebddce6ed574111262f19bb18efbe73b0", 
-                         created_by_id = NmapCertsData.created_by.id, 
-                         updated_by_id = NmapCertsData.created_by.id, 
-                         updated_on = timezone.now())
+    NmapCertsData.created_by = NmapCertsData.get_or_create_user(
+        username='PHSA_User')
+    cert = NmapCertsData(xml_data="sample data",
+                         addresses="10.10.10.10",
+                         orion_id="12345",
+                         not_before=timezone.now(),
+                         not_after=timezone.now(),
+                         md5="57adc7886d93a03d934ae691178748b7",
+                         sha1="ae699d5ebddce6ed574111262f19bb18efbe73b0",
+                         created_by_id=NmapCertsData.created_by.id,
+                         updated_by_id=NmapCertsData.created_by.id,
+                         updated_on=timezone.now())
     cert.save()
     assert cert.sha1 is not "57adc7886d93a03d934ae691178748b7"
     assert cert.md5 is "57adc7886d93a03d934ae691178748b7"
-    assert NmapHistory.objects.filter(md5="57adc7886d93a03d934ae691178748b7").count() is 1
-
-    
-
+    assert NmapHistory.objects.filter(
+        md5="57adc7886d93a03d934ae691178748b7").count() is 1
--- ssl_cert_tracker/tests/test_models.py
+++ ssl_cert_tracker/tests/test_models.py
@@ -12,10 +12,17 @@ django models for the ssl_certificates app
 
 :contact:    ali.rahmat@phsa.ca
 """
+import os
+import sys
+
 from datetime import datetime
 
 from django.test import TestCase
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+
 from ssl_cert_tracker.models import NmapCertsData, NmapHistory
+
 
 class TestSslCertTrackerModels(TestCase):
     """TestSslCertTrackerModels injects different sets of values
@@ -30,11 +37,14 @@ class TestSslCertTrackerModels(TestCase):
 
     def setUp(self):
         """setUp test data"""
-        NmapCertsData.created_by = NmapCertsData.get_or_create_user(username='PHSA_User')
+        NmapCertsData.created_by = NmapCertsData.get_or_create_user(
+            username='PHSA_User')
         NmapCertsData.objects.create(orion_id='12345',
                                      addresses='10.10.10.10',
-                                     not_before=datetime(2018, 1, 21, 14, 32, 24, 38),
-                                     not_after=datetime(2018, 11, 21, 19, 30, 45, 21),
+                                     not_before=datetime(
+                                         2018, 1, 21, 14, 32, 24, 38),
+                                     not_after=datetime(
+                                         2018, 11, 21, 19, 30, 45, 21),
                                      xml_data='',
                                      common_name='A Common Name',
                                      organization_name='A Great IT Place',
@@ -56,13 +66,15 @@ class TestSslCertTrackerModels(TestCase):
         """testing not_before field"""
         expected_object_not_before = self.obj.not_before
         self.assertIsInstance(expected_object_not_before, datetime)
-        self.assertEqual(str(expected_object_not_before), "2018-01-21 14:32:24+00:00")
+        self.assertEqual(str(expected_object_not_before),
+                         "2018-01-21 14:32:24+00:00")
 
     def test_not_after_content_type(self):
         """testing not_after field"""
         expected_object_not_after = self.obj.not_after
         self.assertIsInstance(expected_object_not_after, datetime)
-        self.assertEqual(str(expected_object_not_after), "2018-11-21 19:30:45+00:00")
+        self.assertEqual(str(expected_object_not_after),
+                         "2018-11-21 19:30:45+00:00")
 
     def test_orion_id_content(self):
         """testing orion field"""
@@ -102,20 +114,26 @@ class TestSslCertTrackerModels(TestCase):
     def test_md5(self):
         """testing md5 field"""
         expected_object_md5 = f'{self.obj.md5}'
-        self.assertEqual(expected_object_md5, '57adc7886d93a03d934ae691178748b7')
+        self.assertEqual(expected_object_md5,
+                         '57adc7886d93a03d934ae691178748b7')
 
     def test_sha1(self):
         """testing sha1 field"""
         expected_object_sha1 = f'{self.obj.sha1}'
-        self.assertEqual(expected_object_sha1, 'ae699d5ebddce6ed574111262f19bb18efbe73b0')
+        self.assertEqual(expected_object_sha1,
+                         'ae699d5ebddce6ed574111262f19bb18efbe73b0')
 
     def test_save(self):
         """testing save record"""
-        NmapCertsData.objects.filter(md5="57adc7886d93a03d934ae691178748b7").update(bits="1024")
-        bits_entry = NmapCertsData.objects.filter(md5="57adc7886d93a03d934ae691178748b7")
+        NmapCertsData.objects.filter(
+            md5="57adc7886d93a03d934ae691178748b7").update(bits="1024")
+        bits_entry = NmapCertsData.objects.filter(
+            md5="57adc7886d93a03d934ae691178748b7")
         for fld in bits_entry:
             self.assertEqual(fld.bits, '1024')
-        NmapCertsData.objects.filter(md5="57adc7886d93a03d934ae691178748b7").update(bits="2048")
-        bits_entry = NmapCertsData.objects.filter(md5="57adc7886d93a03d934ae691178748b7")
+        NmapCertsData.objects.filter(
+            md5="57adc7886d93a03d934ae691178748b7").update(bits="2048")
+        bits_entry = NmapCertsData.objects.filter(
+            md5="57adc7886d93a03d934ae691178748b7")
         for fld in bits_entry:
             self.assertEqual(fld.bits, '2048')

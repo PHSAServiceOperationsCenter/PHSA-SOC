5a05f08cd65f8982ddc66a230026cc368024bae8
serbant <serbant@gmail.com>
2018-10-05 12:40:01 -0700

    update the app class for rules_engine

--- rules_engine/apps.py
+++ rules_engine/apps.py
@@ -1,5 +1,34 @@
+"""
+.. _apps:
+
+django apps module for the rules_engine app
+
+:module:    p_soc_auto.rules_engine.apps
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    oct. 5, 2018
+"""
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class RulesEngineConfig(AppConfig):
+    """
+    application class for the rules_engine application
+    """
     name = 'rules_engine'
+    verbose_name = _(
+        'PHSA Service Operations Center Rules Engine Application')
+
+    def ready(self):
+        """
+        in case of circular imports or app not ready place imports
+        in this method
+        """
+        pass

12a9ae833abec0fadd78372932d46b409ade1fce
serbant <serbant@gmail.com>
2018-10-05 13:38:03 -0700

    tesstlib.factories and testlib.fixtures

---
+++ testlib/__init__.py
@@ -0,0 +1,16 @@
+"""
+.. _testlib:
+
+testlib package containing test library modules
+
+:module:    p_soc_auto.testlib
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    oct. 5, 2018
+"""
---
+++ testlib/factories.py
@@ -0,0 +1,5 @@
+'''
+Created on Oct 5, 2018
+
+@author: serban
+'''

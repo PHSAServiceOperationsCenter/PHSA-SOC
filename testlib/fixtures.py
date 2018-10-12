993645f8727be7d2fb3ce929f8bd30cf8585120e
serban teodorescu <serbant@gmail.com>
2018-10-12 15:43:27 -0700

    just a little fixture... but it will grow bigger and stronger every hour

--- testlib/fixtures.py
+++ testlib/fixtures.py
@@ -17,4 +17,16 @@ pytest fixtures for the p_soc_auto applications unit tests
 """
 import pytest
 
+from django.utils import timezone
+
 from .factories import RuleDemoDataFactory
+
+
+@pytest.fixture(params=['not_yet_valid', 'expired', 'warning', 'healthy'])
+def demo_data_for_exp_rule(request, db):
+    if request.param in ['not_yet_valid']:
+        data_datetime_1 = timezone.now() + timezone.timedelta(days=365)
+        data_datetime_2 = timezone.now() + timezone.timedelta(days=366)
+
+    return RuleDemoDataFactory.create(
+        data_datetime_1=data_datetime_1, data_datetime_2=data_datetime_2)

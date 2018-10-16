b6c64d29f5fb86700ad46c61c5227dbfe059f4cb
Ali Rahmat <alirahmat04@yahoo.com>
2018-10-16 08:54:52 -0700

    Updated code...

--- notifications/broadcast.py
+++ notifications/broadcast.py
@@ -31,12 +31,18 @@ class EmailBroadCast(EmailMessage):
     '''
 
     def __init__(self,
-                 notification_pk=None,
-                 connection=None,
-                 subject='default subject',
-                 message='default_message', 
+                 subject='default subject_new',
+                 message='default_message_new', 
                  email_from='ali.rahmat@phsa.ca',
-                 email_to='ali.rahmat@phsa.ca', 
+                 email_to=['ali.rahmat@phsa.ca'],
+                 cc=[],
+                 bcc=[],
+                 connection=None,
+                 attachments=[],
+                 reply_to=['ali.rahmat@phsa.ca',], 
+                 headers=None,
+                 notification_pk=None,
+                 *args,
                  **kwargs
                 ):
 
@@ -52,57 +58,163 @@ class EmailBroadCast(EmailMessage):
         successfull on unsuccessfull email sent
         
         '''
+        subject ="http:10.1.80.24:8000"
         if connection is not None:
-            self.connection = connection
-        if notification_pk is not None:
-            self.obj = Notification.objects.get(pk=pk)
-            email = self.get_defined_email_parameters()
-        else:
-            self.subject = subject
-            self.message = message
-            self.email_from = email_from
-            self.email_to = email_to
-            email = self.get_default_email_parameters()  
+            self.notification_pk = notification_pk
+            self.obj = Notification.objects.get(pk=self.notification_pk)
+            #email = self.get_defined_email_parameters()
+            subject=self.obj.rule_msg
+            body=self.obj.message
+            subject = "http://10.1.80.24:8000/notifications"
+            print (self.obj.rule_msg, subject)
+            # and all the other things from get_defined_email_parameters
 
-        super().__init__(*args, **kwargs)   
-
-        self.send(email)
-
-        if notification_pk is not None:
-            self.post_send_mail_update()
-
-    def get_defined_email_parameters(self):
-        '''
-        creating  defined the email message
-        '''
-        receivers = self.obj.subcribers
-        return {"subject":self.obj.message["rule_msg"],
-                "message":self.obj.message,
-                "from":settings.EMAIL_HOST_USER,
-                "to":receivers,
-                "con":self.connection
-        }
-
-    def get_default_email_parameters(self):
-        '''
-        creating  default email message
-        '''
-        return {"subject":self.subject,
-                "message":self.message,
-                "from":self.email_from,
-                "to":self.email_to
-        }
+        super().__init__(subject,
+                         message,
+                         email_from,
+                         email_to,
+                         bcc,
+                         connection,
+                         attachments,
+                         cc,
+                         reply_to,
+                         headers,
+                         *args, **kwargs)
+        self.send()
+        if notification_pk:
+            post_send_mail_update
 
     def post_send_mail_update(self):
         '''
-        update notification columns upon successful email sent
-        '''
-        self.obj.objects.update(broadcast_on=timezone.now(), escalated_on=timezone.now() + timezone.timedelta(days=7))
+        extend this to include the whole send and update logic
         
+        update notification columns upon successful email sent
+        days, seconds = duration.days, duration.seconds
+        hours = seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = (seconds % 60)
+        return days, hours, minutes, seconds
+        '''
 
-    def send(self, email):
+        Notification.objects.filter(pk=self.notification_pk).update(
+            broadcast_on=timezone.now())
+        # we need to update escalated_on if no one paid
+        #attension to email within
+        # timezone.timedelta(instance.notification_type.escalate_within)
+
+
+        # Notification.objects.filter(pk=self.notification_pk).update(
+        #     escalated_on=timezone.now() +
+        #     timezone.timedelta(instance.notification_type.escalate_within))
+
+
+"""
+.. _utils:
+
+utility  functions for the notification tasks
+
+:module:    p_soc_auto.notification.utils
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    ali.rahmat@phsa.ca
+
+:update:    Oct. 04 2018
+
+"""
+import logging
+from django.utils import timezone
+from django.core.mail import get_connection
+from django.core.mail.message import EmailMessage
+from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
+from django.core.mail import send_mail, BadHeaderError
+from .models import Notification
+
+
+class EmailBroadCast(EmailMessage):
+    '''
+        EmailBroadCast class to Broadcast Email
+    '''
+
+    def __init__(self,
+                 subject='default subject_new',
+                 message='default_message_new', 
+                 email_from='ali.rahmat@phsa.ca',
+                 email_to=['ali.rahmat@phsa.ca'],
+                 cc=[],
+                 bcc=[],
+                 connection=None,
+                 attachments=[],
+                 reply_to=['ali.rahmat@phsa.ca',], 
+                 headers=None,
+                 notification_pk=None,
+                 *args,
+                 **kwargs
+                ):
+
         '''
-        override parent class send method
+        1.pk Notification pk
+        2. con this is smtp connection object assigned
+        from setting.py if passed as None
+        3. subject
+        4.message
+        5.email_from
+        6.email_to
+        7. fields  we need to distinguish this parameter for both
+        successfull on unsuccessfull email sent
+        
         '''
-        super().send(*args, **kwarg)
+        subject ="http:10.1.80.24:8000"
+        if connection is not None:
+            self.notification_pk = notification_pk
+            self.obj = Notification.objects.get(pk=self.notification_pk)
+            #email = self.get_defined_email_parameters()
+            subject=self.obj.rule_msg
+            body=self.obj.message
+            subject = "http://10.1.80.24:8000/notifications"
+            print (self.obj.rule_msg, subject)
+            # and all the other things from get_defined_email_parameters
+
+        super().__init__(subject,
+                         message,
+                         email_from,
+                         email_to,
+                         bcc,
+                         connection,
+                         attachments,
+                         cc,
+                         reply_to,
+                         headers,
+                         *args, **kwargs)
+        self.send()
+        if notification_pk:
+            post_send_mail_update
+
+    def post_send_mail_update(self):
+        '''
+        extend this to include the whole send and update logic
+        
+        update notification columns upon successful email sent
+        days, seconds = duration.days, duration.seconds
+        hours = seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = (seconds % 60)
+        return days, hours, minutes, seconds
+        '''
+
+        Notification.objects.filter(pk=self.notification_pk).update(
+            broadcast_on=timezone.now())
+        # we need to update escalated_on if no one paid
+        #attension to email within
+        # timezone.timedelta(instance.notification_type.escalate_within)
+
+
+        # Notification.objects.filter(pk=self.notification_pk).update(
+        #     escalated_on=timezone.now() +
+        #     timezone.timedelta(instance.notification_type.escalate_within))
+
 

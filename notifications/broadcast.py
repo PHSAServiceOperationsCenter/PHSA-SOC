56eb577d140dbfce881830f176d075944b433d53
PHSAOperationCenter <ali.rahmat@phsa.ca>
2018-10-17 12:47:33 -0700

    fixed arguments..

--- notifications/broadcast.py
+++ notifications/broadcast.py
@@ -15,7 +15,6 @@ utility  functions for the notification tasks
 :update:    Oct. 04 2018
 
 """
-import logging
 from django.utils import timezone
 from django.core.mail.message import EmailMessage
 from django.core.validators import validate_email
@@ -34,11 +33,11 @@ class EmailBroadCast(EmailMessage):
                  subject=settings.DEFAULT_EMAIL_SUBJECT,
                  message=settings.DEFAULT_EMAIL_MESSAGE,
                  email_from=settings.ADMINS[0][1],
-                 email_to=[settings.ADMINS[1][1], settings.ADMINS[2][1]],
+                 email_to=None,
                  cc=settings.DEFAULT_EMAIL_CC,
                  bcc=settings.DEFAULT_EMAIL_BCC,
                  connection=settings.DEFAULT_EMAIL_CONNECTION,
-                 attachments=settings.DEFAULT_EMAIL_ATTACHMENTS,
+                 attachments=None,
                  reply_to=settings.DEFAULT_EMAIL_REPLY_TO,
                  headers=settings.DEFAULT_EMAIL_HEADERS,
                  notification_pk=settings.DEFAULT_NOTIFICATION_PK,
@@ -48,61 +47,69 @@ class EmailBroadCast(EmailMessage):
                 ):
 
         """
-        All default parameters are comming from settings.py        
+        Parameters:
+        subject  (str) if None raise Exception
+        message  (str)  if None raise Exception
+        email_from (str) if None or invalid format raise Exception
+        email_to  (list) if None or empty list or
+                         invalid format raise Exception
+        cc (list) if None ok
+                  if list not empty and invalid format
+                  raise Exception
+        bcc if None ok
+                  if list not empty and invalid format
+                  raise Exception
+        connection if None ok
+        attachments if list not empty and invalid format
+                     raise Exception
+        reply_to (list) if None or empty list or
+                         invalid format raise Exception
+        headers None ok
+        notification_pk None ok else must be a valid pk
+        email_type= 0:subscribers
+                    1:esc.
+                    2: sub & esc.
+                    otherwise:subscribers
+        *args    TBD,
+        **kwargs TBD
         """
-        self.error_flag = False
         self.obj = None
-        if connection is not None:              
-            if Notification.objects.filter(pk=notification_pk).exists():
-                self.notification_pk = notification_pk
-                self.obj = Notification.objects.get(pk = self.notification_pk)
-                subject = self.obj.rule_msg
-                message = self.obj.message
-                if email_type == 0:
-                    email_to = self.obj.subscribers
-                elif email_type == 1: # escalation
-                    email_to = self.obj.escalation_subscribers
-                elif email_type == 2: # both esc, and broadcast
-                    email_to.extend(
-                        self.obj.subscribers).extend(
-                            self.obj.escalation)
-                else: # error
-                    logging.error('Invalid  data %s', 'email_type')
-                    return
-            else:
-                logging.error('Invalid  object %s', 'Notification')
-                return
-
-
-
-        # if notification_pk is not None:
-        #     if subject is None:
-        #         logger.debug('Invalid None parameter %s',
-        #         'Email Subject')
-        #         return
-        #     if message is None:
-        #         logger.debug('Invalid None parameter %s',
-        #         'Email Message')
-        #         return
 
         if notification_pk is None:
             if subject is None or message is None:
-                logging.error('Invalid None parameter %s',
-                'Email Subject/Message')
-                self.error_flag = True
+                raise Exception("Email Subject/Message")
 
             try:
-                validate_email( email_from )
+                validate_email(email_from)
             except ValidationError:
-                logging.error('Invalid Email %s', 'Email From')
-                self.error_flag = True
+                raise Exception("Invalid Email From")
 
             self.validate_email_types(email_to)
-            self.validate_list_types(cc, bcc,attachments)
+            self.validate_list_types([cc, bcc])
             self.validate_email_types(reply_to)
+            if attachments is not None:
+                self.validate_list_types(attachments)
 
-        if self.error_flag:
-            return
+        if email_to is None:
+            email_to = [name for name, address in settings.ADMINS]
+
+        if Notification.objects.filter(pk=notification_pk).exists():
+            self.notification_pk = notification_pk
+            self.obj = Notification.objects.get(pk=self.notification_pk)
+            subject = self.obj.rule_msg
+            message = self.obj.message
+            if email_type == 0:
+                email_to = self.obj.subscribers
+            elif email_type == 1: # escalation
+                email_to = self.obj.escalation_subscribers
+            elif email_type == 2: # both esc, and broadcast
+                email_to.extend(
+                    self.obj.subscribers).extend(
+                        self.obj.escalation)
+            else: # error
+                raise Exception('Invalid  data %s', 'email_type')
+        else:
+            raise Exception('Invalid  data %s', 'email_type')
 
         super().__init__(subject,
                          message,
@@ -116,7 +123,7 @@ class EmailBroadCast(EmailMessage):
                          headers,
                          *args, **kwargs)
 
-    def post_send_mail_update(self):
+    def update_notification_timestamps(self):
         """
         extend this to include the whole send and update logic
         update notification columns upon successful email sent
@@ -136,37 +143,25 @@ class EmailBroadCast(EmailMessage):
         # Notification.objects.filter(pk=self.notification_pk).update(
         #     escalated_on=timezone.now() +
         #     timezone.timedelta(instance.notification_type.escalate_within))
-    def validate_list_types(self, cc, bcc, attachments):
+    def validate_list_types(self, items):
         """
         validates if arguments are list instance
         """
-        if not isinstance(cc, (list, )):
-            logging.error('Not a list instance %s', 'cc')
-            self.error_flag = True
+        for item in items:
+            if not isinstance(item, (list, )):
+                raise Exception(str(item) + ": Not a list instance")
 
-        if self.error_flag == False:
-            if not isinstance(bcc, (list, )):
-                logging.error('Not a list instance %s', 'bcc')
-                self.error_flag = True
-
-        if not isinstance(attachments, (list, )):
-            logging.error('Not a list instance %s', 'attachments')
-            self.error_flag = True
-
-    def validate_email_types(self, email):
+    def validate_email_types(self, email_to):
         """
         validates if argument are email format
         """
         if not isinstance(email, (list, )):
-            logging.error('Not a list instance %s', email)
-            self.error_flag = True
+            raise Exception(str(email_to) + ": Not a list instance")
         elif len(email_to) == 0:
-            logging.error('Invalid Email %s', email)
-            self.error_flag = True
+            raise Exception(str(email_to) + ": Invalid Email")
         else:
             for email in email_to:
                 try:
                     validate_email(email)
                 except ValidationError:
-                    logging.error('Invalid Email %s', email)
-                    self.error_flag = True
+                    raise Exception(str(email) + ": Invalid Email")

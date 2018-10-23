7a202f618de1f65d72b7fed17d4595ee4ed313c0
serban teodorescu <serbant@gmail.com>
2018-10-23 16:03:55 -0700

    fix orion_id bug; was using bad data
    extend nmap probes with retry and logging (in progress)

--- ssl_cert_tracker/tasks.py
+++ ssl_cert_tracker/tasks.py
@@ -17,8 +17,9 @@ django models for the ssl_certificates app
 import logging
 
 import xml.dom.minidom
-from django.core.exceptions import ObjectDoesNotExist
 from celery import shared_task
+from celery.utils.log import get_task_logger
+from celery.exceptions import MaxRetriesExceededError
 from libnmap.process import NmapProcess
 from orion_integration.lib import OrionSslNode
 
@@ -26,27 +27,37 @@ from .db_helper import insert_into_certs_data
 
 from .utils import process_xml_cert
 
-logging.basicConfig(filename='p_soc_auto.log', level=logging.DEBUG)
+logger = get_task_logger(__name__)
 
 
-@shared_task(rate_limit='0.5/s', queue='nmap')
-def go_node(node_id, node_address):
-    """Celery worker for each orion node"""
-    xml_data = ""
-    count = 0
-    while count < 5:
-        count = count + 1
-        try:
-            nmap_task = NmapProcess(node_address, options='--script ssl-cert')
-            nmap_task.run()
-            xml_data = nmap_task.stdout
-            break
-        except Exception as ex:
-            logging.error("Error proceesing xml_cert message:%s", ex)
+class NmapError(Exception):
+    """
+    raise on nmap failure
+    """
+    pass
+
+
+@shared_task(
+    rate_limit='0.5/s', queue='nmap', autoretry_for=(NmapError,),
+    max_retries=3, retry_backoff=True)
+def go_node(orion_id, node_address):
+    """
+    collect SSL certificate data as returned by nmap and save it to the
+    database
+    """
+    xml_data = None
+    try:
+        nmap_task = NmapProcess(node_address, options='--script ssl-cert')
+        nmap_task.run()
+        xml_data = nmap_task.stdout
+    except MaxRetriesExceededError as error:
+        logger.error('')
+    except Exception as ex:
+        logging.error("Error proceesing xml_cert message:%s", ex)
 
     if count < 5:
         doc = xml.dom.minidom.parseString(xml_data)
-        json = process_xml_cert(node_id, doc)
+        json = process_xml_cert(orion_id, doc)
         if json["md5"] is None:
             msg = "Error Proceesing data or missing hash value"
             logging.error("Error proceesing xml_cert message:%s", msg)
@@ -56,11 +67,17 @@ def go_node(node_id, node_address):
 
 @shared_task(queue='ssl')
 def getnmapdata():
-    """Celery worker to capture all nodes then it delegate each node to different worker"""
-    node_obj = OrionSslNode.nodes()
-    for node in node_obj:
+    """
+    get the orion node information that will be used for nmap probes
+
+    #TODO: rename this to get_orion_nodes()
+    #TODO: replace the go_node.delay() loop with a celery group
+    """
+    for node in OrionSslNode.nodes():
         try:
-            go_node.delay(node.id, node.ip_address)
-            logging.info("Success proceesing :%s", node.ip_address)
-        except ObjectDoesNotExist as ex:
-            logging.error("Error proceesing node ip message:%s", ex)
+            go_node.delay(node.orion_id, node.ip_address)
+            logging.info('queued nmap call for node %s' % node.node_caption)
+        except Exception as ex:
+            logging.error(
+                'cannot queue nmap call for node %s: %s' % (node.node_caption,
+                                                            str(ex)))

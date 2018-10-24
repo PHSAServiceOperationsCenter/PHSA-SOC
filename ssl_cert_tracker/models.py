76714d03193153f5bfffe6cf38a6a3d1bb778c6c
PHSAOperationCenter <ali.rahmat@phsa.ca>
2018-10-24 07:12:18 -0700

    fixed ssl cert error....

--- ssl_cert_tracker/models.py
+++ ssl_cert_tracker/models.py
@@ -13,10 +13,15 @@ django models for the ssl_certificates app
 :contact:    ali.rahmat@phsa.ca
 
 """
+import logging
+import xml.dom.minidom
+from libnmap.process import NmapProcess
 from django.utils import timezone
 from django.db import models
+from django.utils.dateparse import parse_datetime
 from p_soc_auto_base.models import BaseModel
-
+from .utils import process_xml_cert
+from .utils import validate
 
 class NmapCertsData(BaseModel, models.Model):
     """NmapCertsData. Model struture for NMap result response. """
@@ -26,10 +31,12 @@ class NmapCertsData(BaseModel, models.Model):
     not_after = models.DateTimeField(null=True, blank=True)
     xml_data = models.TextField()
     common_name = models.CharField(max_length=100, blank=True, null=True,
-                                   help_text="A unique title for common_name")
+                                   help_text="A unique title \
+                                   for common_name")
     organization_name = models.CharField(max_length=100,
                                          blank=True, null=True,
-                                         help_text="A unique title for organization_name")
+                                         help_text="A unique \
+                                         title for organization_name")
     country_name = models.CharField(max_length=100, blank=True, null=True)
     sig_algo = models.CharField(max_length=100, blank=True, null=True)
     name = models.CharField(max_length=100, blank=True, null=True)
@@ -62,8 +69,87 @@ class NmapCertsData(BaseModel, models.Model):
                                       xml_data=self.xml_data)
                     obj.save()
                 else:
-                    NmapHistory.objects.filter(orion_id=self.orion_id).update(status="found",
-                                                                              retreived=timezone.now())
+                    NmapHistory.objects.filter(
+                        orion_id=self.orion_id).update(
+                            status="found",
+                            retreived=timezone.now())
+
+    @staticmethod
+    def retreive_cert_data(node_id, node_address):
+        """retreive_cert_data. """
+
+        xml_data = ""
+        try:
+            nmap_task = \
+            NmapProcess(node_address, options='--script ssl-cert')
+            nmap_task.run()
+            xml_data = nmap_task.stdout
+            doc = xml.dom.minidom.parseString(xml_data)
+            json = process_xml_cert(node_id, doc)
+            
+            if json["md5"] is not None:
+                # insert_into_certs_data check if record
+                # already exist then update otherwise insert
+                print (json)
+                NmapCertsData.created_by = \
+                NmapCertsData.get_or_create_user(username='PHSA_User')
+                cert_status = NmapCertsData.get_cert_state(json["orion_id"],
+                                                           json["md5"]
+                                                           )
+                if cert_status not in [0, 1, 2]:
+                    msg = "failure"
+                    logging.error(
+                        "Error accessing django model \
+                        NmapCertsData get_cert_state:%s",
+                        msg)
+                    return
+
+                if cert_status == 0:
+                    # un-changed, update retreived column in cert hist
+                    db_certs_hist = NmapHistory()
+                    db_certs_hist.update_retreived_cert_hist(json["md5"])
+                    msg = "un-changed, update retrieved column in cert hist!"
+                    logging.info("certsHist data update:.... %s", msg)
+                    return
+
+                if cert_status == 1: #new reord
+                    db_certs = NmapCertsData()
+                    msg = "Unchanged!"
+                    logging.info("Cert data info:.... %s", msg)
+
+                else:
+                    # cert changed cert_status == 2
+                    db_certs = NmapCertsData.get_cert_handle(json["orion_id"])
+                    msg = "Changed!"
+                    logging.info("Cert data info:.... %s", msg)
+
+                db_certs.orion_id = json["orion_id"]
+                db_certs.addresses = json["addresses"]
+                if cert_status in [1]:
+                    # new record
+                    db_certs.created_by_id = NmapCertsData.created_by.id
+                    db_certs.updated_by_id = NmapCertsData.created_by.id
+                db_certs.updated_on = timezone.now()
+
+                if validate(json["not_before"]):
+                    db_certs.not_before = parse_datetime(json["not_before"])
+
+                if validate(json["not_after"]):
+                    db_certs.not_after = parse_datetime(json["not_after"])
+
+                db_certs.common_name = json["common_name"]
+                db_certs.organization_name = json["organization_name"]
+                db_certs.country_name = json["country_name"]
+                db_certs.sig_algo = json["sig_algo"]
+                db_certs.name = json["name"]
+                db_certs.bits = json["bits"]
+                db_certs.md5 = json["md5"]
+                db_certs.sha1 = json["sha1"]
+                db_certs.xml_data = json["xml_data"]
+                db_certs.save()
+        except Exception as ex:
+            logging.error("Error proceesing xml_cert message:%s", ex)
+
 
     def save(self, *args, **kwargs):
         super(NmapCertsData, self).save(*args, **kwargs)
@@ -71,24 +157,29 @@ class NmapCertsData(BaseModel, models.Model):
         self.update_cert_history(pk_val)
 
     def __str__(self):
-        return 'O: %s, CN: %s' % (self.organization_name, self.common_name)
+        return 'O: %s, CN: %s' % (self.organization_name,
+                                  self.common_name)
 
     @staticmethod
     def get_cert_handle(o_id):
-        """get_cert_handle """
+        """
+        get_cert_handle
+        """
         return NmapCertsData.objects.get(orion_id=o_id)
 
     @staticmethod
     def get_cert_state(o_id, hash_md5):
-        """orion_id_exist """
+        """
+        Check if we already have this cert in our database
+        """
         if NmapCertsData.objects.filter(orion_id=o_id).count() == 0:
             return_code = 1  # new reord
-        elif NmapCertsData.objects.filter(orion_id=o_id, md5=hash_md5).count() == 0:
+        elif NmapCertsData.objects.filter(orion_id=o_id,
+                                          md5=hash_md5).count() == 0:
             return_code = 2  # cert changed
         else:  # cert has not changed
-            NmapHistory.objects.filter(orion_id=o_id,
-                                       md5=hash_md5).update(
-                retreived=timezone.now())
+            NmapHistory.objects.filter(
+                orion_id=o_id, md5=hash_md5).update(retreived=timezone.now())
             return_code = 0
         return return_code
 
@@ -106,16 +197,26 @@ class NmapHistory(models.Model):
     retreived = models.DateTimeField(null=True, blank=True)
     xml_data = models.TextField()
 
-    def updateRetreivedCertHist(self, md5):
-        obj = NmapHistory.objects.filter(md5 = self.md5)
-        obj = NmapHistory(retreived=timezone.now())
-        obj.save()
+    def update_retreived_cert_hist(self, md5):
+        """
+        Updating Retreived column in Cert Hist Table
+        """
+        NmapHistory.objects.filter(
+            md5=md5).update(retreived=timezone.now())
 
     def __str__(self):
-        return str(self.cert_id)
+        return str(self.md5)
 
 
 class NmapCertsScript(BaseModel, models.Model):
-    """NmapCertsScript. Model struture for NMap scripts. https://nmap.org/nsedoc/"""
-    name = models.CharField(max_length=100, unique=True, blank=True, null=True)
-    command = models.CharField(max_length=100, blank=True, null=True)
+    """
+    NmapCertsScript
+    Model struture for NMap scripts. https://nmap.org/nsedoc/
+    """
+    name = models.CharField(max_length=100,
+                            unique=True,
+                            blank=True,
+                            null=True)
+    command = models.CharField(max_length=100,
+                               blank=True,
+                               null=True)
--- ssl_cert_tracker/tasks.py
+++ ssl_cert_tracker/tasks.py
@@ -16,51 +16,30 @@ django models for the ssl_certificates app
 
 import logging
 
-import xml.dom.minidom
 from django.core.exceptions import ObjectDoesNotExist
 from celery import shared_task
-from libnmap.process import NmapProcess
 from orion_integration.lib import OrionSslNode
-
-from .db_helper import insert_into_certs_data
-
-from .utils import process_xml_cert
+from .models import NmapCertsData
 
 logging.basicConfig(filename='p_soc_auto.log', level=logging.DEBUG)
 
-
 @shared_task(rate_limit='0.5/s', queue='nmap')
 def go_node(node_id, node_address):
-    """Celery worker for each orion node"""
-    xml_data = ""
-    count = 0
-    while count < 5:
-        count = count + 1
-        try:
-            nmap_task = NmapProcess(node_address, options='--script ssl-cert')
-            nmap_task.run()
-            xml_data = nmap_task.stdout
-            break
-        except Exception as ex:
-            logging.error("Error proceesing xml_cert message:%s", ex)
-
-    if count < 5:
-        doc = xml.dom.minidom.parseString(xml_data)
-        json = process_xml_cert(node_id, doc)
-        if json["md5"] is None:
-            msg = "Error Proceesing data or missing hash value"
-            logging.error("Error proceesing xml_cert message:%s", msg)
-        else:
-            insert_into_certs_data(json)
-
+    """Serban, This method no longer needed
+    If I remove it then go_node tasks still appears
+    in celery worker tasks list
+    """
+    pass
 
 @shared_task(queue='ssl')
 def getnmapdata():
-    """Celery worker to capture all nodes then it delegate each node to different worker"""
+    """Celery worker to capture all nodes then
+     it delegate each node to different worker
+    """
     node_obj = OrionSslNode.nodes()
     for node in node_obj:
         try:
-            go_node.delay(node.id, node.ip_address)
+            NmapCertsData.retreive_cert_data(node.id, node.ip_address)
             logging.info("Success proceesing :%s", node.ip_address)
         except ObjectDoesNotExist as ex:
             logging.error("Error proceesing node ip message:%s", ex)

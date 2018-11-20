f67039bab6a939e8136c5d946b970395e8ccfc2b
Serban Teodorescu <serban.teodorescu@phsa.ca>
2018-11-20 14:54:40 -0800

    library module citrus_borg.lib
    add process_borg_message() function for manipulating
    ControlUp windows events
    
    add get_ip_for_host_name() to deal with host name and ip address
    combinations as returned by Winlogbeat

---
+++ citrus_borg/lib.py
@@ -0,0 +1,172 @@
+"""
+.. _lib:
+
+functions and classes for the citrus_borg app
+
+:module:    citrus_borg.lib
+
+:copyright:
+
+    Copyright 2018 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    nov. 19, 2018
+
+"""
+import collections
+import socket
+
+from django.utils.dateparse import parse_duration
+
+
+def get_ip_for_host_name(host_name=None, ip_list=None):
+    """
+    :returns: the IP address that goes with a known host name or ``None``
+
+    :arg str host_name: the host name as reported from external sources
+
+    :arg list ip_list: a list of ip addresses for a host as returned from
+                       external sources
+
+    :raises: 
+
+        :exception:`<ValueError>` if either argument is missing
+
+        :exception:`<TypeError>` if ip_list is not a ``list`` or ``tuple``
+
+    the input are the values for the name and ip keys in the host dictionary
+    returned by the winlogbeat + logstash combination for a given windows
+    event. Here is a sample:
+
+    host: {
+        'name': 'baby_d',
+        'mac': ['02:00:4c:4f:4f:50', '9e:b6:d0:8a:23:df', 'ae:b6:d0:8a:23:df',
+                '9c:b6:d0:8a:23:df', '9c:b6:d0:8a:23:e0', '02:15:03:a1:a2:5e'],
+        'ip': ['fe80::449b:87fb:5758:b29', '169.254.11.41',
+               'fe80::bc38:afcd:34ba:8de2', '169.254.141.226',
+               'fe80::5181:28ba:b614:957a', '169.254.149.122',
+               'fe80::441f:c81b:f69b:e22b', '10.42.27.105',
+               'fe80::e947:1c6c:3ce9:ec12', '169.254.236.18',
+               'fe80::dd4c:609f:d278:2d75', '172.24.70.33'],
+        'id': 'e4ee2cbd-baa7-4e97-abfc-afd5a8e46730',
+        'architecture': 'x86_64',
+        'os': {
+            'version': '10.0', 'build': '17134.407', 'platform': 'windows',
+            'family': 'windows'
+        }
+    }
+
+    the function loops through the :arg:`<ip_list>` and returns
+    the one ip address that resolves to the :arg:`<host_name>`
+
+    """
+    if host_name is None:
+        raise ValueError('host_name argument is mandatory')
+
+    host_name = str(host_name)
+
+    if ip_list is None:
+        raise ValueError('ip_list argument is mandatory')
+
+    if not isinstance(ip_list, (list, tuple)):
+        raise TypeError(
+            'bad type %s for %s. must be a list or tuple'
+            % (type(ip_list), ip_list)
+        )
+
+    for ip_address in ip_list:
+        try:
+            _host_name, _alias_list, _ip_list = \
+                socket.gethostbyaddr(ip_address)
+            if host_name in _host_name:
+                return ip_address
+            continue
+        except:  # noqa: E722
+            continue
+
+    return None
+
+
+def process_borg_message(message=None):
+    """
+    this is the string when success
+
+    Successful logon: to resource: PMOffice - CST Brokered by device: \
+        PHSACDCTX42
+
+    Test Details:
+    ----------------------
+    Latest Test Result: True
+    Storefront Connection Time: 00:00:03.1083110
+    Receiver Startup: 00:00:01.1673405
+    Connection Time: 00:00:00.4353095
+    Logon Time: 00:00:04.0380058
+    Logoff Time: 00:00:05.3581214
+
+
+    and here's the bad honey
+
+    Failed logon to resource: PMOffice - CST
+    Failure reason: The Connection failed during session logon.
+    Failure details:
+
+    Test Output:
+    ----------------------
+    13:51:17 - Starting storefront connection
+    13:51:17 - Initialising storefront
+    13:51:17 - Requesting resources
+    13:51:19 - Received resources
+    13:51:19 - Resource found, requesting ICA file
+    13:51:20 - Connecting to: 104.170.202.132:1494
+    13:51:20 - ICA file received
+    13:51:20 - Initialising received
+    13:51:21 - Connecting received
+    13:51:21 - Connect received
+    13:51:22 - On disconnect
+    13:51:22 - Total connection time: 0
+
+
+    """
+    if message is None:
+        raise ValueError('message argument is mandatory')
+
+    message = message.split('\n')
+
+    borg_message = collections.namedtuple(
+        'BorgMessage',
+        ['state', 'broker', 'test_result', 'storefront_connection_duration',
+         'receiver_startup_duration', 'connection_achieved_duration',
+         'logon_achieved_duration', 'logoff_achieved_duration',
+         'failure_reason', 'failure_details'])
+
+    borg_message.state = message[0].split()[0]
+    if borg_message.state in ['Successful']:
+        borg_message.broker = message[0].split()[-1]
+        borg_message.test_result = bool(message[4].split()[-1])
+        borg_message.storefront_connection_duration = \
+            parse_duration(message[5].split()[-1])
+        borg_message.receiver_startup_duration = \
+            parse_duration(message[6].split()[-1])
+        borg_message.connection_achieved_duration = \
+            parse_duration(message[7].split()[-1])
+        borg_message.logon_achieved_duration = \
+            parse_duration(message[8].split()[-1])
+        borg_message.logoff_achieved_duration = \
+            parse_duration(message[9].split()[-1])
+        borg_message.failure_reason = None
+        borg_message.failure_details = None
+
+    else:
+        borg_message.failure_reason = message[1].split(': ')[1]
+        borg_message.failure_details = '\n'.join(message[-12:-1])
+        borg_message.broker = None
+        borg_message.test_result = False
+        borg_message.storefront_connection_duration = None
+        borg_message.receiver_startup_duration = None
+        borg_message.connection_achieved_duration = None
+        borg_message.logon_achieved_duration = None
+        borg_message.logoff_achieved_duration = None
+
+    return borg_message

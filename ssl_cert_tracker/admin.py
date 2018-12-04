0b369234f672d0e2a249ce426a2fcd99c984ae6a
serban teodorescu <serban.teodorescu@phsa.ca>
2018-12-04 12:12:04 -0800

    enable auto population for created_by, updated_by

--- ssl_cert_tracker/admin.py
+++ ssl_cert_tracker/admin.py
@@ -15,6 +15,7 @@ django admin for the ssl_cert_tracker app
 :updated:    sep. 21, 2018
 """
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from rangefilter.filter import DateRangeFilter
 
 from simple_history.admin import SimpleHistoryAdmin
@@ -27,6 +28,52 @@ from p_soc_auto_base.admin import BaseAdmin
 
 @admin.register(Subscription)
 class Subscription(BaseAdmin, admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        overload
+        admin.ModelAdmin.formfield_for_foreignkey(
+            self, db_field, request, **kwargs)
+        """
+        if db_field.name in ['created_by', 'updated_by', ]:
+            kwargs['queryset'] = get_user_model().objects.\
+                filter(username=request.user.username)
+            kwargs['initial'] = kwargs['queryset'].get()
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def add_view(self, request, form_url='', extra_context=None):
+        """
+        overload to populate the user fields from the request object
+        """
+        data = request.GET.copy()
+        data['created_by'] = request.user
+        data['updated_by'] = request.user
+        request.GET = data
+
+        return super().add_view(
+            request, form_url=form_url, extra_context=extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """
+        overload to populate updated_by from the request object
+        """
+        data = request.GET.copy()
+        data['updated_by'] = request.user
+        request.GET = data
+
+        return super().change_view(
+            request, object_id, form_url=form_url, extra_context=extra_context)
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        overload to make sure that some fields are always readonly
+        """
+        if obj is not None:
+            return self.readonly_fields + \
+                ('created_by', 'created_on', 'updated_on')
+
+        return self.readonly_fields
+
     def has_add_permission(self, request, obj=None):
         if request.user.is_superuser:
             return True
@@ -44,7 +91,7 @@ class Subscription(BaseAdmin, admin.ModelAdmin):
                     'updated_by']
     list_editable = ['enabled', 'emails_list', 'template_dir',
                      'template_name', 'template_prefix']
-    readonly_fields = ['created_on', 'updated_on', ]
+    readonly_fields = ('created_on', 'updated_on', )
 
 
 class SSLCertTrackerBaseAdmin(BaseAdmin, admin.ModelAdmin):

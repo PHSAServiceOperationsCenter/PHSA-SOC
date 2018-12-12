3f06539419ae14ba9e0054c1dee6ba3a7d4973c3
serban <serbant@gmail.com>
2018-12-12 15:25:20 -0800

    default citrus_borg alarms for dead bots/sites set to 10 minutes

--- p_soc_auto/settings.py
+++ p_soc_auto/settings.py
@@ -291,8 +291,8 @@ SERVER_PORT = '8080'
 
 # settings specific to the citrus_borg application
 CITRUS_BORG_SERVICE_USER = 'citrus-borg'
-CITRUS_BORG_DEAD_BOT_AFTER = timezone.timedelta(minutes=12)
-CITRUS_BORG_DEAD_SITE_AFTER = timezone.timedelta(minutes=12)
+CITRUS_BORG_DEAD_BOT_AFTER = timezone.timedelta(minutes=10)
+CITRUS_BORG_DEAD_SITE_AFTER = timezone.timedelta(minutes=10)
 CITRUS_BORG_DEAD_BROKER_AFTER = timezone.timedelta(hours=24)
 CITRUS_BORG_NOT_FORGOTTEN_UNTIL_AFTER = timezone.timedelta(hours=72)
 CITRUS_BORG_IGNORE_EVENTS_OLDER_THAN = timezone.timedelta(hours=72)

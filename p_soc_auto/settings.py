50fc38b3838d3fa549d467f34e45c24e5a09a1b7
serban teodorescu <serbant@gmail.com>
2019-01-22 16:07:24 -0800

    put the SSL nmap options under settings

--- p_soc_auto/settings.py
+++ p_soc_auto/settings.py
@@ -179,7 +179,7 @@ DATABASES = {
         'USER': 'orion_aux_db_user',
         'PASSWORD': 'orion_aux_db_password',
         #	'SA_PASSWORD': "orion_aux_db_password123',
-        'HOST': '10.66.6.9',
+        'HOST': 'localhost',
         'PORT': '',
 
         'OPTIONS': {
@@ -326,34 +326,15 @@ CITRUS_BORG_FAILED_LOGONS_PERIOD = timezone.timedelta(hours=12)
 CITRUS_BORG_NO_NEWS_IS_GOOD_NEWS = False
 
 DYNAMIC_PREFERENCES = {
-
-    # a python attribute that will be added to model instances with preferences
-    # override this if the default collide with one of your models
-    # attributes/fields
     'MANAGER_ATTRIBUTE': 'preferences',
-
-    # The python module in which registered preferences will be searched
-    # within each app
     'REGISTRY_MODULE': 'dynamic_preferences_registry',
-
-    # Allow quick editing of preferences directly in admin list view
-    # WARNING: enabling this feature can cause data corruption if multiple users
-    # use the same list view at the same time, see
-    # https://code.djangoproject.com/ticket/11313
     'ADMIN_ENABLE_CHANGELIST_FORM': True,
-
-    # Customize how you can access preferences from managers. The default is to
-    # separate sections and keys with two underscores. This is probably not a settings you'll
-    # want to change, but it's here just in case
     'SECTION_KEY_SEPARATOR': '__',
-
-    # Use this to disable caching of preference. This can be useful to debug
-    # things
     'ENABLE_CACHE': True,
-
-    # Use this to disable checking preferences names. This can be useful to
-    # debug things
     'VALIDATE_NAMES': True,
 }
 
 NMAP_SERVICE_USER = 'nmap_user'
+
+SSL_PROBE_OPTIONS = '-Pn -p %s --script ssl-cert'
+SSL_DEFAULT_PORT = 443

be1899b8d0d0643f776f5c2ccf314b6524a45cfe
Serban Teodorescu <serban.teodorescu@phsa.ca>
2019-01-30 17:24:23 -0800

    orion_flash.lib module

---
+++ orion_flash/lib.py
@@ -0,0 +1,20 @@
+"""
+.. _lib:
+
+library module for the orion_flash app
+
+:module:    orion_flash.lib
+
+:copyright:
+
+    Copyright 2018 - 2019 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    Jan. 30, 2019
+
+"""
+from logging import getLogger
+
+LOG = getLogger('orion_flash')

d3211669f3d205b2becea386092db8da74569b0b
serban teodorescu <serbant@gmail.com>
2019-02-06 15:57:55 -0800

    orion_flash.admin quick enable so we can reverse urls to it

--- orion_flash/admin.py
+++ orion_flash/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    UntrustedSslAlert, ExpiresSoonSslAlert, ExpiredSslAlert, InvalidSslAlert,
+)
+
+
+@admin.register(UntrustedSslAlert)
+class UntrustedSslAlertAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExpiresSoonSslAlert)
+class ExpiresSoonSslAlertAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExpiredSslAlert)
+class ExpiredSslAlertAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(InvalidSslAlert)
+class InvalidSslAlertAdmin(admin.ModelAdmin):
+    pass

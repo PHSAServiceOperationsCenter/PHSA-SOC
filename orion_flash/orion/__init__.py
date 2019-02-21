e419bed8f7b3e99cb7ccc2e9cdc640bfe01796af
serban teodorescu <serbant@gmail.com>
2019-02-20 17:00:19 -0800

    orion utility package and copy properties module

---
+++ orion_flash/orion/__init__.py
@@ -0,0 +1,21 @@
+"""
+.. _orion:
+
+orion commands
+
+:module:    p_soc_auto.orion_flash.orion
+
+:copyright:
+
+    Copyright 2018 - 2019 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    Feb. 20, 2019
+
+orion script library
+====================
+
+utility commands for manipulating orion entities between servers
+"""

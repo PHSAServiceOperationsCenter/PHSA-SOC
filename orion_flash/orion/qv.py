dd9e8651200e43ee95bb9eedc6dfe60b04b9dcc6
serban teodorescu <serbant@gmail.com>
2019-02-25 11:10:45 -0800

    debugged custom properties methods. also enhanced so that custom property possible values are copied between servers

--- orion_flash/orion/qv.py
+++ orion_flash/orion/qv.py
@@ -15,13 +15,32 @@ queries and verbs for Orion SDK
 :updated:    Feb. 22, 2019
 
 """
-CUSTOM_PROPS_QUERY = (
+ALL_CUSTOM_PROPS_QUERY = (
     'SELECT Table, Field, DataType, MaxLength, StorageMethod,'
     ' Description, TargetEntity, Mandatory, Default, DisplayName,'
-    ' Uri, InstanceSiteId FROM Orion.CustomProperty'
+    ' Uri, InstanceSiteId '
+    'FROM Orion.CustomProperty'
+)
+FILTERED_CUSTOM_PROPS_QUERY = (
+    'SELECT Table, Field, DataType, MaxLength, StorageMethod,'
+    ' Description, TargetEntity, Mandatory, Default, DisplayName,'
+    ' Uri, InstanceSiteId '
+    'FROM Orion.CustomProperty '
+    'WHERE Table=@table AND Field=@field'
 )
 CUSTOM_PROPS_VERB = 'CreateCustomProperty'
+CUSTOM_PROPS_VALS_VERB = 'CreateCustomPropertyWithValues'
 CUSTOM_PROPS_INVOKE_ARGS = [
     'Field', 'Description', 'DataType', 'MaxLength', 'ValidRange', 'Parser',
     'Header', 'Alignment', 'Format', 'Units', 'Usages', 'Mandatory', 'Default',
 ]
+CUSTOM_PROPS_VALS_INVOKE_ARGS = [
+    'Field', 'Description', 'DataType', 'MaxLength', 'ValidRange', 'Parser',
+    'Header', 'Alignment', 'Format', 'Units', 'Values', 'Usages', 'Mandatory',
+    'Default',
+]
+VALUES_FOR_CUSTOM_PROP_QUERY = (
+    'SELECT Value '
+    'FROM Orion.CustomPropertyValues '
+    'WHERE Table=@table AND Field=@field'
+)

cd803c8b3882b3a508afd9949cc6d0649d0c86cc
serban <serbant@gmail.com>
2019-04-10 16:17:53 -0700

    create file systems and start adding code for sending exchange messages from local site bots

---
+++ mail_borg/logger.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 10, 2019
+
+@author: serban
+'''
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
---
+++ mail_borg/mailer.py
@@ -0,0 +1,17 @@
+"""
+.. _mailer:
+
+mail module for exchange monitoring borg bots
+
+:module:    mail_borg.mailer
+
+:copyright:
+
+    Copyright 2018 - 2019 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    apr. 10, 2019
+
+"""

62f5d0def14b56225927837894ead438d84de524
serban <serbant@gmail.com>
2019-05-30 14:03:40 -0700

    fixx data collection the mail_collector application

--- mail_collector/tasks.py
+++ mail_collector/tasks.py
@@ -31,65 +31,50 @@ def store_mail_data(body):
     """
     grab the exchange events from rabbitmq and dump it to the database
     """
-    result = chain(extract_exchange_data_from_event.s(body),
-                   update_borg_info.s(), create_exchange_event.s())()
-
-    return result
-
-
-@shared_task(queue='mail_collector', task_serializer='pickle')
-def extract_exchange_data_from_event(body):
-    """
-    process the windows log events collected from the exchange monitoring
-    client to a structure suitable for passing on to the relevant django
-    model instance
-
-    :arg dict body: the event data
-
-        it arrives as a JSON structure from the
-        winlogbeat + logstash + rabbitMQ collection mechanism and it is
-        decoded by `<citrus_borg.consumers>`
-
-    :raises: :exception:`<mail_collector.exceptions.BadEventDataError>`
-    """
     try:
-        return process_borg(body, LOGGER)
+        exchange_borg = process_borg(body, LOGGER)
     except:  # @IgnorePep8
         raise exceptions.BadEventDataError(
             'cannot process event data from %s' % body)
 
-
-@shared_task(queue='mail_collector', task_serializer='pickle')
-def update_borg_info(exchange_borg):
-    """
-    create or update the host instance in
-    :class:`<citrus_borg.models.WinlogbeatHost>`
-
-    :raises: :exception:`<mail_collector.exceptions.BadHostDataFromEventError>`
-    """
     try:
-        exchange_borg.source_host = WinlogbeatHost.get_or_create_from_borg(
+        source_host = WinlogbeatHost.get_or_create_from_borg(
             exchange_borg)
     except:  # @IgnorePep8
         raise exceptions.BadHostDataFromEventError(
             'cannot update borg host info from %s' % str(exchange_borg))
 
-    return exchange_borg
+    event_data = exchange_borg.mail_borg_message[0]
 
-
-@shared_task(queue='mail_collector', task_serializer='pickle')
-def create_exchange_event(exchange_borg):
-    """
-    create the event data instance in the mail_collector models
-    """
-    event_data = exchange_borg.borg_message[0]._asdict()
-
-    event = models.MailBotLogEvent(**event_data)
-    event.save()
-
-    if exchange_borg.borg_message[1]:
-        mail_data = exchange_borg.borg_message[1]._asdict()
-        event = models.MailBotMessage(event=event, **mail_data)
+    try:
+        event = models.MailBotLogEvent(
+            source_host=source_host,
+            event_status=event_data.event_status,
+            event_type=event_data.event_type,
+            event_message=event_data.event_message,
+            event_body=event_data.event_body,
+            event_exception=event_data.event_exception)
         event.save()
+    except Exception as error:
+        raise error
 
-    return event
+    if exchange_borg.mail_borg_message[1]:
+        mail_data = exchange_borg.mail_borg_message[1]
+        try:
+            event = models.MailBotMessage(
+                event=event,
+                mail_message_identifier=mail_data.mail_message_identifier,
+                sent_from=mail_data.sent_from, sent_to=mail_data.sent_to,
+                received_from=mail_data.received_from,
+                received_by=mail_data.received_by,
+                mail_message_created=mail_data.mail_message_created,
+                mail_message_sent=mail_data.mail_message_sent,
+                mail_message_received=mail_data.mail_message_received)
+            event.save()
+        except Exception as error:
+            raise error
+
+        return ('created exchange monitoring event from email message %s'
+                % event.mail_message_identifier)
+
+    return ('created exchange monitoring event %s' % event.uuid)

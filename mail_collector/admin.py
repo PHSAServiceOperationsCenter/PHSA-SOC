bf76783c307487e7cd4923d8a921b005cf764934
serban <serbant@gmail.com>
2019-06-03 15:05:18 -0700

    intermediate iteration for event admin pages

--- mail_collector/admin.py
+++ mail_collector/admin.py
@@ -48,12 +48,12 @@ class MailBotLogEventAdmin(MailBotAdmin, admin.ModelAdmin):
     admin forms for mail bot log events
     """
     list_display_links = ('uuid',)
-    list_display = ('uuid', 'event_type', 'event_status', 'event_message',
-                    'event_exception', 'source_host',
-                    'show_site', 'event_registered_on')
+    list_display = ('uuid', 'event_group_id', 'event_type', 'event_status',
+                    'mail_account', 'event_message', 'event_exception',
+                    'source_host', 'show_site', 'event_registered_on')
     readonly_fields = ('uuid', 'event_type', 'event_status', 'source_host',
                        'event_registered_on', 'show_site', 'event_message',
-                       'event_exception',)
+                       'event_exception', 'event_group_id', 'mail_account',)
     list_filter = ('event_status', 'event_type',
                    'source_host__host_name', 'source_host__site__site',
                    ('event_registered_on', DateTimeRangeFilter),)
@@ -64,6 +64,18 @@ class MailBotLogEventAdmin(MailBotAdmin, admin.ModelAdmin):
         """
         return obj.source_host.site.site
     show_site.short_description = 'site'
+
+    def get_actions(self, request):
+        """
+        we don't need no actions for these events, they are controlled
+        entirely by background processes
+        """
+        actions = super().get_actions(request)
+        if 'enable_selected' in actions:
+            del actions['enable_selected']
+        if 'disable_selected' in actions:
+            del actions['disable_selected']
+        return actions
 
 
 @admin.register(MailHost)
@@ -94,9 +106,15 @@ class MailBotMessageAdmin(MailBotAdmin, admin.ModelAdmin):
     admin ofrms for exchange monitoring events that include a mail message
     """
     list_display_links = ('event_uuid',)
-    list_display = ('event_uuid',
-                    'show_site', )
-    readonly_fields = ('event_uuid', )
+    list_display = ('event_uuid', 'event_group_id', 'mail_message_identifier',
+                    'event_type', 'event_status', 'mail_account', 'event_message',
+                    'source_host', 'show_site', )
+    readonly_fields = ('event_uuid', 'event_group_id', 'event_type',
+                       'event_status', 'mail_message_identifier', 'show_site',
+                       'event_message', 'source_host', 'event_body',
+                       'sent_from', 'sent_to', 'received_from', 'received_by',
+                       'mail_message_created', 'mail_message_sent',
+                       'mail_message_received', 'mail_account')
 
     list_filter = ('event__event_status', 'event__event_type',
                    'event__source_host__host_name',
@@ -113,3 +131,43 @@ class MailBotMessageAdmin(MailBotAdmin, admin.ModelAdmin):
     def event_uuid(self, obj):
         return obj.event.uuid
     event_uuid.short_description = 'UUID'
+
+    def event_group_id(self, obj):
+        return obj.event.event_group_id
+    event_group_id.short_description = 'Session ID'
+
+    def event_type(self, obj):
+        return obj.event.event_type
+    event_type.short_description = 'Event Type'
+
+    def event_status(self, obj):
+        return obj.event.event_status
+    event_status.short_description = 'Event Status'
+
+    def event_message(self, obj):
+        return obj.event.event_message
+    event_message.short_description = 'Event Message'
+
+    def source_host(self, obj):
+        return obj.event.source_host
+    source_host.short_description = 'Event Source Host'
+
+    def event_body(self, obj):
+        return obj.event.event_body
+    event_body.short_description = 'Raw Event Data'
+
+    def mail_account(self, obj):
+        return obj.event.mail_account
+    mail_account.short_description = 'Mail Account'
+
+    def get_actions(self, request):
+        """
+        we don't need no actions for these events, they are controlled
+        entirely by background processes
+        """
+        actions = super().get_actions(request)
+        if 'enable_selected' in actions:
+            del actions['enable_selected']
+        if 'disable_selected' in actions:
+            del actions['disable_selected']
+        return actions

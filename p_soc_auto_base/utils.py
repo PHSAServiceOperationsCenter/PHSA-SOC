5a4b96510a4b31d0e80c317ab37cf11c7d40aa46
Serban Teodorescu <serban.teodorescu@phsa.ca>
2019-06-11 15:51:38 -0700

    class RelativeTimeDelta will deliver a past moment in time relative to a reference moment and going back a specified interval

--- p_soc_auto_base/utils.py
+++ p_soc_auto_base/utils.py
@@ -89,3 +89,82 @@ def _make_aware(datetime_input, use_timezone=timezone.utc, is_dst=False):
 
     return timezone.make_aware(
         datetime_input, timezone=use_timezone, is_dst=is_dst)
+
+
+class RelativeTimeDelta():
+    """
+    quick and dirty way of calculating a ``datetime.datetime`` object in the
+    past relative to another ``datetime.datetime`` object (the reference
+    moment).
+
+    in most cases the reference moment is the value returned by
+    :method:`<datetime.dateime.now>` but sometimes we need something else
+    """
+    @staticmethod
+    def _now(now):
+        """
+        static method for the now reference moment
+        """
+        if now is None:
+            now = timezone.now()
+
+        if not isinstance(now, timezone.datetime):
+            raise TypeError(
+                'Invalid object type %s, was expecting datetime' % type(now))
+
+        return now
+
+    @staticmethod
+    def _time_delta(time_delta=None, **kw_time_delta):
+        """
+        return a proper ``datetime.timedelta`` object. if a dictionary is
+        provided instead, try to build the object from the dictionary
+
+        :raises:
+
+            ``exceptions.TypeError`` when :arg:`<time_delta>`` is not of the
+            proper type or if the input dictionary cannot be used as argument
+            for ``datetime.timedelta``
+
+        :param time_delta: default ``None``
+        :type time_delta: ``datetime.timedelta``
+        """
+
+        if time_delta is None:
+            try:
+                time_delta = timezone.timedelta(**kw_time_delta)
+            except TypeError as error:
+                raise error
+
+        if not isinstance(time_delta, timezone.timedelta):
+            raise TypeError(
+                'Invalid object type %s, was expecting timedelta'
+                % type(time_delta))
+
+        return time_delta
+
+    @classmethod
+    def time_delta(cls, time_delta=None, now=None):
+        """
+        return a relative moment in the past when the interval is specified
+        as a ``datetime.timedelta`` object
+        """
+        if time_delta is None:
+            raise TypeError(
+                'Invalid object type %s, was expecting timedelta'
+                % type(time_delta))
+
+        return RelativeTimeDelta._now(now) \
+            - RelativeTimeDelta._time_delta(time_delta)
+
+    @classmethod
+    def time_delta_from_dict(cls, now=None, **kw_time_delta):
+        """
+        return a relative moment in the past when the interval is specified
+        as a dictionary suitable for ``datetime.timedelta`` objects
+        """
+        if not kw_time_delta:
+            raise ValueError('you must specify the tiem interval')
+
+        return RelativeTimeDelta._now(now) \
+            - RelativeTimeDelta._time_delta(**kw_time_delta)

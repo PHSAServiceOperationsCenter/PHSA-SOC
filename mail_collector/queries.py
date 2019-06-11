b89312e5b571d467676c1195439e9f30b752c45a
Serban Teodorescu <serban.teodorescu@phsa.ca>
2019-06-11 15:53:58 -0700

    mail_collector.queries.dead_bodies() will return a queryset of objects not seen after a given moment in time

---
+++ mail_collector/queries.py
@@ -0,0 +1,67 @@
+"""
+.. _queries:
+
+query classes and functions for the mail_collector app
+
+:module:    mail_collector.queries
+
+:copyright:
+
+    Copyright 2018 - 2019 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    Jun. 11, 2019
+
+"""
+import datetime
+
+from django.apps import apps
+
+from mail_collector.models import (
+    ExchangeServer, ExchangeDatabase, MailBotLogEvent, MailBotMessage,)
+
+from citrus_borg.dynamic_preferences_registry import get_preference
+from citrus_borg.locutus.communication import GroupBy, _group_by
+from orion_flash.api import (
+    url_annotate as _url_annotate,
+    details_url_annotate as _details_url_annotate,
+)
+from p_soc_auto_base.utils import RelativeTimeDelta
+
+
+def dead_bodies(data_source, filter_exp,
+                not_before=None, url_annotate=False):
+    """
+    return instances not seen before a moment in time
+
+    :param data_source: the data source in 'app_label.modelname' format
+    :type data_source: str
+    :param filter_exp: the field and lookup to use for filtering
+
+        for example, 'last_updated__lte' will filter on a field named
+        last_updated using a less than or equal lookup
+    :type filter_exp: str
+    :param not_before: the moment in time used for filtering
+    :type not_before: datetime.datetime
+    :param url_annotate: do we also add the absolute url for each object?
+    :type url_annotate: bool
+    """
+    if not_before is None:
+        not_before = RelativeTimeDelta.time_delta(
+            time_delta=get_preference('exchange__server_error'))
+
+    if not isinstance(not_before, datetime.datetime):
+        raise TypeError(
+            'Invalid object type %s, was expecting datetime'
+            % type(not_before))
+
+    queryset = apps.get_model(data_source).objects.filter(enabled=True)
+
+    queryset = queryset.filter(**{filter_exp: not_before})
+
+    if url_annotate:
+        queryset = _url_annotate(queryset)
+
+    return queryset

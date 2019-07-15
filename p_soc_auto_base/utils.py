84cb0eb455dbeb910d01394937632d6679730b3b
serban teodorescu <serbant@gmail.com>
2019-07-15 12:56:02 -0700

    move utility functions to p_soc_base.utils where all the utility functions should be living these days

--- p_soc_auto_base/utils.py
+++ p_soc_auto_base/utils.py
@@ -12,19 +12,141 @@ utility classes and functions for the p_soc_auto project
 
 :contact:    serban.teodorescu@phsa.ca
 
-:update:    Feb. 1, 2019
+:update:    Jul. 15, 2019
 
 """
 import logging
+import socket
 
 from django.apps import apps
 from django.core.exceptions import FieldError
+from django.conf import settings
+from django.db.models import F, Value, TextField, URLField
+from django.db.models.functions import Cast, Concat
+from django.db.models.query import QuerySet
 from django.utils import timezone
 
 from ssl_cert_tracker.models import Subscription
 from ssl_cert_tracker.lib import Email
 
 LOGGER = logging.getLogger('django')
+
+
+class UnknownDataTargetError(Exception):
+    """
+    raise when trying to upload to an unknown data model
+    """
+
+
+class DataTargetFieldsAttributeError(Exception):
+    """
+    raise when the target model doesn't have a qs_fields attribute
+    """
+
+
+def get_model(destination):
+    """
+    get the model where the data is to be saved
+    """
+    try:
+        return apps.get_model(*destination.split('.'))
+    except Exception as err:
+        raise UnknownDataTargetError from err
+
+
+def get_queryset_values_keys(model):
+    """
+    this function returns a list of keys that will be passed to
+    :method:`<django.db.models.query.QuerySet.values>`
+
+    :returns: a `list of field names
+
+        note that these field names are for the queryset, **not for the
+        model that is the source of thew queryset**, because a queryset
+        can contain fields defined via annotations and/or aggregations
+
+    :raises: :exception:`<DataTargetFieldsAttributeError>`
+    """
+    if not hasattr(model, 'qs_fields'):
+        raise DataTargetFieldsAttributeError(
+            'model %s is missing the qs_fields attribute'
+            % model._meta.model_name)
+
+    return model.qs_fields
+
+
+def url_annotate(queryset):
+    """
+    annotate a queryset with the admin url of the underlying model
+
+    :arg queryset: the :class:`<django.db.models.query.QuerySet>` object
+
+    :returns: the queryset with an additional field named url
+
+    :raises:
+
+        :exception:`<TypeError>` if the argument is nnot a queryset
+    """
+    if not isinstance(queryset, QuerySet):
+        raise TypeError(
+            'bad type %s for object %s' % (type(queryset), queryset))
+
+    # get the object metadata from an (the first) element of the queryset
+    # we need the app_label, model_name, and primary key field name so
+    # that we can build the url value
+    obj_sample = queryset.model._meta
+
+    return queryset.annotate(url_id=Cast(obj_sample.pk.name, TextField())).\
+        annotate(url=Concat(
+            Value(settings.SERVER_PROTO), Value('://'),
+            Value(socket.getfqdn()),
+            Value(':'), Value(settings.SERVER_PORT),
+            Value('/admin/'),
+            Value(obj_sample.app_label), Value(
+                '/'), Value(obj_sample.model_name),
+            Value('/'), F('url_id'), Value('/change/'),
+            output_field=URLField()))
+
+
+def details_url_annotate(
+        queryset, app_path=None,
+        model_path=None, param_name=None, param_lookup_name=None):
+    """
+    annotate a queryset with an admin link to related records
+    http://10.2.50.35:8080/admin/citrus_borg/winlogevent/?source_host__host_name=bccss-t450s-02
+    """
+    if not isinstance(queryset, QuerySet):
+        raise TypeError(
+            'bad type %s for object %s' % (type(queryset), queryset))
+
+    if param_name is None:
+        raise ValueError(
+            'cannot build URL parameters without a parameter name')
+
+    if param_lookup_name is None:
+        raise ValueError(
+            'cannot build URL parameters without a parameter value')
+
+    obj_sample = queryset.model._meta
+
+    if app_path is None:
+        app_path = obj_sample.app_label
+
+    if model_path is None:
+        model_path = obj_sample.model_name
+
+    try:
+        return queryset.annotate(details_url=Concat(
+            Value(settings.SERVER_PROTO), Value('://'),
+            Value(socket.getfqdn()),
+            Value(':'), Value(settings.SERVER_PORT),
+            Value('/admin/'),
+            Value(app_path), Value(
+                '/'), Value(model_path),
+            Value('/?'), Value(param_name), Value('='), F(param_lookup_name),
+            output_field=URLField()))
+    except Exception as error:
+        raise error
 
 
 def remove_duplicates(sequence=None):

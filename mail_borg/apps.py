b0accef3246f166c532be8e2fba6d9c02098e291
serban teodorescu <serbant@gmail.com>
2019-08-29 15:40:14 -0700

    messing with docstrings. why isn't autodoc being updated?

--- mail_borg/apps.py
+++ mail_borg/apps.py
@@ -1,9 +1,9 @@
 """
 .. _apps:
 
-django apps module for the mail_collector app
+**Django app class for the mail_collector application**
 
-:module:    p_soc_auto.mail_collector.apps
+:module:    mail_collector.apps
 
 :copyright:
 
@@ -14,12 +14,19 @@ django apps module for the mail_collector app
 
 :updated:    May 27, 2019
 
+
+This module is used to register the :ref:`mail_collector` with the Django
+server.
+
 """
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
 class MailCollectorConfig(AppConfig):
+    """
+    mail collector application class
+    """
     name = 'mail_collector'
     verbose_name = _(
         'PHSA Service Operations Center Exchange Monitoring Application')

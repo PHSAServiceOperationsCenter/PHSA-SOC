0dd44d8e21e9ca7b182b5c92a19363f91e7b31ef
serban teodorescu <serbant@gmail.com>
2019-09-05 15:30:18 -0700

    document dynamic prefs for mail_collector

--- citrus_borg/dynamic_preferences_registry.py
+++ citrus_borg/dynamic_preferences_registry.py
@@ -1,8 +1,6 @@
 """
 .. _dynamic_preferences_registry:
 
-dynamic preferences for the citrus_borg app
-
 :module:    citrus_borg.dynamic_preferences_registry
 
 :copyright:
@@ -14,18 +12,40 @@ dynamic preferences for the citrus_borg app
 
 :updated:    jan. 3, 2019
 
+This module is used to provide run-time configurable settings.
+
+Although it is defined as part of the :ref:`Citrus Borg Application`, it
+is shared across all the applications in the :ref:`SOC Automation Project`.
+
+Each class in the module is a dynamic preference and each dynamic preference
+can be configured via the `Global preferences admin interface 
+<../../../admin/dynamic_preferences/globalpreferencemodel/>`_ using the value
+of the ``verbose_name`` attribute of the class. 
+
+To access the value of a given dynamic setting, use the :ref:`get_preference`
+function. For example:
+
+.. ipython::
+
+        In [1]: from citrus_borg.dynamic_preferences_registry import get_preference
+
+        In [2]: get_preference('exchange__report_interval')
+        Out[2]: datetime.timedelta(1, 43200)
+
+        In [3]:
+
 """
 from django.conf import settings
 from django.utils import timezone
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
-
+from dynamic_preferences.preferences import Section
+from dynamic_preferences.registries import global_preferences_registry
 from dynamic_preferences.types import (
     BooleanPreference, StringPreference, DurationPreference, IntPreference,
     LongStringPreference, FloatPreference,
 )
-from dynamic_preferences.preferences import Section
-from dynamic_preferences.registries import global_preferences_registry
+
 
 # pylint: disable=E1101,C0103
 #=========================================================================
@@ -34,8 +54,6 @@ from dynamic_preferences.registries import global_preferences_registry
 #
 # C0103: asr PEP8 module level variables are constants and should be upper-case
 #=========================================================================
-
-
 citrus_borg_common = Section(
     'citrusborgcommon', verbose_name=_('citrus borg common settings').title())
 
@@ -81,65 +99,93 @@ exchange = Section('exchange',
 @global_preferences_registry.register
 class ExchangeExpireEvents(DurationPreference):
     """
-    expire events older than preference
+    Dynamic preferences class controlling how old an event collected by the
+    :ref:`Mail Collector Application` must be before it will be marked
+    as expired
+
+    :access_key: 'exchange__expire_events`
     """
     section = exchange
     name = 'expire_events'
     default = timezone.timedelta(hours=36)
+    """default setting value"""
     required = True
     verbose_name = _('Exchange events older than')
+    """verbose name of this dynamic preference"""
 
 
 @global_preferences_registry.register
 class ExchangeReportingInterval(DurationPreference):
     """
-    expire events older than preference
+    Dynamic preferences class controlling the time interval used by the
+    :ref:`Mail Collector Application` for generating reports
+
+    :access_key: 'exchange__report_interval'
     """
     section = exchange
     name = 'report_interval'
     default = timezone.timedelta(hours=12)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('exchange reporting interval')
+    """verbose name of this dynamic preference"""
 
 
 @global_preferences_registry.register
 class ExchangeReportErrorLevel(StringPreference):
     """
-    configure the consumer for exchange monitoring events
+    Dynamic preferences class controlling the error level used by the
+    :ref:`Mail Collector Application` when generating reports
+
+    :access_key: 'exchange__report_level'
     """
     section = exchange
     name = 'report_level'
     default = 'INFO'
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('Error level for all Exchange reports')
+    """verbose name of this dynamic preference"""
     help_text = format_html(
         "{}", _('a report does npt really have an error level but we need'
                 ' a value here than can be empty, i.e. no level in order'
-                ' to resue existing mail templates'))
+                ' to reuse existing mail templates'))
 
 
 @global_preferences_registry.register
 class ExchangeDeleteExpired(BooleanPreference):
     """
-    delete expired eventss preference
+    Dynamic preferences class controlling whether expired event collected by
+    the :ref:`Mail Collector Application` will be deleted
+
+    :access_key: `exchange__delete_expired`
     """
     section = exchange
     name = 'delete_expired'
     default = True
+    """default value for this dynamic preference"""
     required = False
     verbose_name = _('Delete expired Exchange events')
+    """verbose name for this dynamic preference"""
 
 
 @global_preferences_registry.register
 class ExchangeSendEmptyAlerts(BooleanPreference):
     """
-    delete expired eventss preference
+    Dynamic preferences class controlling whether emails are being sent
+    by the :ref:`Mail Collector Application` when an alert is not being
+    raised. This the opposite of the `No news is good news 
+    <https://www.phrases.org.uk/bulletin_board/45/messages/366.html>`_ phrase
+
+    :access_key: 'exchange__empty_alerts'
     """
     section = exchange
     name = 'empty_alerts'
     default = False
+    """default value for this dynamic preference"""
     required = False
     verbose_name = _('Always Send Email Alerts')
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('send email notifications even when there are no alerts'))
 
@@ -147,25 +193,37 @@ class ExchangeSendEmptyAlerts(BooleanPreference):
 @global_preferences_registry.register
 class ExchangeDefaultErrorLevel(StringPreference):
     """
-    configure the consumer for exchange monitoring events
+    Dynamic preferences class controlling the error level used by the
+    :ref:`Mail Collector Application` when sending alerts for which no
+    error level was specified
+
+    :access_key: 'exchange__default_level`
     """
     section = exchange
     name = 'default_level'
     default = 'WARNING'
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('Default error level for Exchange alerts')
+    """verbose name for this dynamic preference"""
 
 
 @global_preferences_registry.register
 class ExchangeEventSource(StringPreference):
     """
-    configure the consumer for exchange monitoring events
+    Dynamic preferences class that provides the key used by the
+    :ref:`Mail Collector Application` data collection to identify
+    events of interest
+
+    :access_key: 'exchange__source'
     """
     section = exchange
     name = 'source'
     default = 'BorgExchangeMonitor'
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('Windows Logs Source for Exchange Monitoring Events ')
+    """verbose name for this dynamic preference"""
     help_text = _(
         'This is a list of values. Use "," to separate the list items')
 
@@ -173,13 +231,19 @@ class ExchangeEventSource(StringPreference):
 @global_preferences_registry.register
 class ExchangeServerWarn(DurationPreference):
     """
-    warn about exchange servers if not seen for the specified interval
+    Dynamic preferences class controlling how long the
+    :ref:`Mail Collector Application` will wait before raising a warning
+    about an Exchange entity not providing Exchange services
+
+    :access_key: 'exchange__server_warn'
     """
     section = exchange
     name = 'server_warn'
     default = timezone.timedelta(hours=2)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('exchange server warnings after').title()
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('raise warning about an Exchange server if it has not been'
                 ' seen for longer than this time period'))
@@ -188,13 +252,19 @@ class ExchangeServerWarn(DurationPreference):
 @global_preferences_registry.register
 class ExchangeServerError(DurationPreference):
     """
-    warn about exchange servers if not seen for the specified interval
+    Dynamic preferences class controlling how long the
+    :ref:`Mail Collector Application` will wait before raising an error
+    about an Exchange entity not providing Exchange services
+
+    :access_key: 'exchange__server_warn'
     """
     section = exchange
     name = 'server_error'
     default = timezone.timedelta(hours=6)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('exchange server errors after').title()
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('raise error about an Exchange server if it has not been'
                 ' seen for longer than this time period'))
@@ -203,13 +273,19 @@ class ExchangeServerError(DurationPreference):
 @global_preferences_registry.register
 class ExchangeDeadBotWarn(DurationPreference):
     """
-    warn about exchange servers if not seen for the specified interval
+    Dynamic preferences class controlling how long the
+    :ref:`Mail Collector Application` will wait before raising a warning
+    about an Exchange monitoring bot not sending any Exchange events
+
+    :access_key: 'exchange__bot_warn'
     """
     section = exchange
     name = 'bot_warn'
     default = timezone.timedelta(hours=2)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('exchange client bot warnings after').title()
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('raise warning about an Exchange client bot if it has not been'
                 ' seen for longer than this time period'))
@@ -218,13 +294,19 @@ class ExchangeDeadBotWarn(DurationPreference):
 @global_preferences_registry.register
 class ExchangeNilDuration(DurationPreference):
     """
-    0 hours interval
+    Dynamic preferences class providing a 0 duration for internal use
+    within the :ref:`Mail Collector Application`
+
+    :access_key: 'exchange__nil_duration'
+
     """
     section = exchange
     name = 'nil_duration'
     default = timezone.timedelta(hours=0)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('nil duration').title()
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('some filters will not work if there is no time interval'
                 ' baked in. if we want to reuse these filters without'
@@ -234,13 +316,19 @@ class ExchangeNilDuration(DurationPreference):
 @global_preferences_registry.register
 class ExchangeDeadBotError(DurationPreference):
     """
-    warn about exchange servers if not seen for the specified interval
+    Dynamic preferences class controlling how long the
+    :ref:`Mail Collector Application` will wait before raising an error
+    about an Exchange monitoring bot not sending any Exchange events
+
+    :access_key: 'exchange__bot_error'
     """
     section = exchange
     name = 'bot_error'
     default = timezone.timedelta(hours=6)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('exchange client bot errors after').title()
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('raise error about an Exchange client bot if it has not been'
                 ' seen for longer than this time period'))
@@ -249,13 +337,18 @@ class ExchangeDeadBotError(DurationPreference):
 @global_preferences_registry.register
 class ExchangeDefaultError(DurationPreference):
     """
-    warn about exchange servers if not seen for the specified interval
-    """
+    Dynamic preferences class controlling how long the
+    :ref:`Mail Collector Application` will wait before raising an error
+    about unspecified itesm
+
+    :access_key: 'exchange__bot_error'"""
     section = exchange
     name = 'default_error'
     default = timezone.timedelta(hours=2)
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('exchange errors after (default)').title()
+    """verbose name for this dynamic preference"""
     help_text = format_html(
         "{}", _('fallback error configuration for all exchange entities'))
 
@@ -268,8 +361,10 @@ class CitrusBorgEventSource(StringPreference):
     section = citrus_borg_events
     name = 'source'
     default = 'ControlUp Logon Monitor'
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('Windows Logs Source for Citrix Events ').title()
+    """verbose name for this dynamic preference"""
     help_text = _(
         'This is a list of values. Use "," to separate the list items')
 
@@ -282,6 +377,7 @@ class EmailFromWhenDebug(StringPreference):
     section = email_prefs
     name = 'from_email'
     default = 'serban.teodorescu@phsa.ca'
+    """default value for this dynamic preference"""
     required = True
     verbose_name = _('originating email address when in DEBUG mode').title()
 
@@ -755,7 +851,13 @@ class LogonReportsInterval(DurationPreference):
 
 def get_preference(key=None):
     """
-    get the preference
+    get the current value of a dynamic preference
+    (also known as a dynamic setting)
+
+    :arg str key: the accessor key for the preference
+        it follows this format 'section__preference_name`; default is ``None``
+
+    :raises: :exc:`Exception` if the key argument cannot be found
     """
     preferences = global_preferences_registry.manager().load_from_db()
     try:
--- docs/source/mail_collector/dynamic_settings.rst
+++ docs/source/mail_collector/dynamic_settings.rst
@@ -1,2 +1,100 @@
 Mail Collector Dynamic Settings
 ===============================
+
+.. automodule:: citrus_borg.dynamic_preferences_registry
+
+get_preference
+--------------
+
+.. autofunction:: citrus_borg.dynamic_preferences_registry.get_preference
+
+Expire Exchange events
+----------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeExpireEvents
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Delete expired Exchange events
+------------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeDeleteExpired
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Exchange reports interval
+-------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeReportingInterval
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Exchange reports error level
+----------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeReportErrorLevel
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Exchange empty alerts
+---------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeSendEmptyAlerts
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Exchange default error level
+----------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeDefaultErrorLevel
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Exchange event source
+---------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeEventSource
+   :members: default, verbose_name
+   :show-inheritance:
+   
+Exchange entities warning threshold
+-----------------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeServerWarn
+   :members: default, verbose_name
+   :show-inheritance:
+
+Exchange entities error threshold
+---------------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeServerError
+   :members: default, verbose_name
+   :show-inheritance:
+
+Exchange bot warning threshold
+---------------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeDeadBotWarn
+   :members: default, verbose_name
+   :show-inheritance:
+
+Exchange nil duration
+---------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeNilDuration
+   :members: default, verbose_name
+   :show-inheritance:
+
+Exchange bot error threshold
+---------------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeDeadBotError
+   :members: default, verbose_name
+   :show-inheritance:
+
+Exchange default error threshold
+---------------------------------
+
+.. autoclass:: citrus_borg.dynamic_preferences_registry.ExchangeDefaultError
+   :members: default, verbose_name
+   :show-inheritance:

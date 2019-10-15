81e1248d03752290d77ed65263b9b071c6c7445a
serban <serbant@gmail.com>
2019-10-15 15:41:40 -0700

    fix a bug and start docs for citrus_borg.admin

--- citrus_borg/admin.py
+++ citrus_borg/admin.py
@@ -8,12 +8,12 @@
 
 :copyright:
 
-    Copyright 2018 Provincial Health Service Authority
+    Copyright 2018 - 2019 Provincial Health Service Authority
     of British Columbia
 
 :contact:    serban.teodorescu@phsa.ca
 
-:updated:    nov. 126, 2018
+:updated:    Oct. 15, 2019
 
 """
 from django.contrib import admin
@@ -21,25 +21,28 @@ from django.contrib.auth import get_user_model
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
 from p_soc_auto_base.admin import BaseAdmin
-
 from .models import (
     WinlogEvent, WinlogbeatHost, KnownBrokeringDevice, BorgSite,
     BorgSiteNotSeen, WinlogbeatHostNotSeen, KnownBrokeringDeviceNotSeen,
     AllowedEventSource, CitrixHost
 )
+from mail_collector.models import ExchangeConfiguration
 
 
 class CitrusBorgBaseAdmin(BaseAdmin, admin.ModelAdmin):
     """
-    base admin class for the citrus_borg application
+    Base :class:`django.contrib.admin.ModelAdmin` class for all the other classes
+    in this module
     """
     list_per_page = 50
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         """
-        overload
-        admin.ModelAdmin.formfield_for_foreignkey(
-            self, db_field, request, **kwargs)
+        override
+        :meth:`django.contrib.admin.ModelAdmin.formfield_for_foreignkey`
+
+        provide specialized drop-down values for `created_by`, `updated_by`,
+        `exchange_client_config`, and `site` `ForeignKey` fields.
         """
         if db_field.name in ['created_by', 'updated_by', ]:
             kwargs['queryset'] = get_user_model().objects.\
@@ -49,11 +52,20 @@ class CitrusBorgBaseAdmin(BaseAdmin, admin.ModelAdmin):
         if db_field.name in ['site', ]:
             kwargs['queryset'] = BorgSite.objects.filter(enabled=True)
 
+        if db_field.name in ['exchange_client_config', ]:
+            kwargs['queryset'] = ExchangeConfiguration.objects.\
+                filter(enabled=True)
+            kwargs['initial'] = ExchangeConfiguration.get_default()
+
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def add_view(self, request, form_url='', extra_context=None):
         """
-        overload to populate the user fields from the request object
+        overload
+        :meth:`django.contrib.admin.ModelAdmin.add_view`
+
+        pre-populate `created_by` and `updated_by` from the :attr:`user` attribute
+        of the `request` object.
         """
         data = request.GET.copy()
         data['created_by'] = request.user
@@ -65,7 +77,11 @@ class CitrusBorgBaseAdmin(BaseAdmin, admin.ModelAdmin):
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         """
-        overload to populate updated_by from the request object
+        overload
+        :meth:`django.contrib.admin.ModelAdmin.change_view`
+
+        pre-populate `updated_by` from the :attr:`user` attribute
+        of the `request` object.
         """
         data = request.GET.copy()
         data['updated_by'] = request.user
@@ -76,7 +92,11 @@ class CitrusBorgBaseAdmin(BaseAdmin, admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):  # @UnusedVariable
         """
-        overload to make sure that some fields are always readonly
+        overload
+        :meth:`django.contrib.admin.ModelAdmin.get_readonly_fields`
+
+        Make sure that the 'created_by', 'created_on', and 'updated_on' fields
+        are always read only.
         """
         if obj is not None:
             return self.readonly_fields + \
@@ -88,7 +108,8 @@ class CitrusBorgBaseAdmin(BaseAdmin, admin.ModelAdmin):
 @admin.register(AllowedEventSource)
 class AllowedEventSourceAdmin(CitrusBorgBaseAdmin, admin.ModelAdmin):
     """
-    admin class for event sources
+    :class:`django.contrib.admin.ModelAdmin` class for the
+    :class:`citrus_borg.models.AllowedEventSource`
     """
     list_display = ('source_name', 'enabled', 'notes',
                     'updated_on', 'updated_by')
@@ -99,7 +120,8 @@ class AllowedEventSourceAdmin(CitrusBorgBaseAdmin, admin.ModelAdmin):
 @admin.register(BorgSite)
 class BorgSiteAdmin(CitrusBorgBaseAdmin, admin.ModelAdmin):
     """
-    admin class for borg sites
+    :class:`django.contrib.admin.ModelAdmin` class for the
+    :class:`citrus_borg.models.BorgSite`
     """
     list_display = ('site', 'enabled', 'notes',
                     'last_seen', 'excgh_last_seen', 'updated_on', 'updated_by')
@@ -131,20 +153,36 @@ class BorgSiteAdmin(CitrusBorgBaseAdmin, admin.ModelAdmin):
 @admin.register(BorgSiteNotSeen)
 class BorgSiteNotSeenAdmin(BorgSiteAdmin):
     """
-    admin forms for sites that have not been seen for a while
+    :class:`django.contrib.admin.ModelAdmin` class for the
+    :class:`citrus_borg.models.BorgSiteNotSeen`
     """
 
 
 @admin.register(KnownBrokeringDevice)
 class KnownBrokeringDeviceAdmin(CitrusBorgBaseAdmin, admin.ModelAdmin):
     """
-    admin class for citrix session hosts
+    :class:`django.contrib.admin.ModelAdmin` class for the
+    :class:`citrus_borg.models.KnownBrokeringDevice`
     """
 
     def has_add_permission(self, request):  # @UnusedVariable
+        """
+        overload
+        :meth:`django.contrib.admin.has_add_permission`
+
+        Nobody is allowed to create any instance using this class.
+        All the data is maintained by background processes.
+        """
         return False
 
     def has_delete_permission(self, request, obj=None):  # @UnusedVariable
+        """
+        overload
+        :meth:`django.contrib.admin.has_delete_permission`
+
+        Nobody is allowed to delete any instance using this class.
+        All the data is maintained by background processes.
+        """
         return False
 
     list_display = ('broker_name',  'enabled', 'last_seen', 'created_on',)
@@ -166,13 +204,15 @@ class WinlogbeatHostAdmin(CitrusBorgBaseAdmin, admin.ModelAdmin):
     admin forms for citrix bots
     """
     list_display = ('host_name', 'ip_address', 'orion_id', 'enabled', 'site',
-                    'resolved_fqdn', 'last_seen', 'created_on',)
-    list_editable = ('site', 'enabled',)
+                    'exchange_client_config', 'resolved_fqdn', 'last_seen',
+                    'created_on',)
+    list_editable = ('site', 'enabled', 'exchange_client_config',)
     readonly_fields = ('host_name', 'ip_address', 'resolved_fqdn', 'last_seen',
                        'created_on', 'orion_id',)
-    list_filter = ('site__site', 'enabled',
+    list_filter = ('site__site', 'enabled', 'exchange_client_config__is_default',
                    ('last_seen', DateTimeRangeFilter),)
-    search_fields = ('site__site', 'host_name', 'ip_address')
+    search_fields = ('site__site', 'host_name', 'ip_address',
+                     'exchange_client_config__config_name',)
 
     def has_add_permission(self, request):  # @UnusedVariable
         """

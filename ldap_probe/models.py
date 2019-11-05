b8fb2c7ab4d0d480990e557860cabadeb95e5122
serban <serbant@gmail.com>
2019-11-05 15:56:08 -0800

    create and load the ldap_probe application

--- ldap_probe/models.py
+++ ldap_probe/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+"""
+ldap_probe.models
+-----------------
 
-# Create your models here.
+This module contains the :class:`django.db.models.Model` models for the
+:ref:`Domain Controllers Monitoring Application`.
+
+:copyright:
+
+    Copyright 2018 - 2019 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    Nov. 5, 2019
+
+"""
+import logging
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from p_soc_auto_base.models import BaseModel
+
+
+LOGGER = logging.getLogger('ldap_probe_log')
+
+
+class NonOrionNode(BaseModel, models.Model):
+    """
+    """
+
+
+class LdapProbeLog(models.Model):
+    """
+    """

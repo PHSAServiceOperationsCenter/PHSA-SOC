966a3d6863099f63554c908b81e24bcbdc6ff567
serban <serbant@gmail.com>
2019-11-12 15:57:12 -0800

    ldap_probe custom exceptions

---
+++ ldap_probe/exceptions.py
@@ -0,0 +1,34 @@
+"""
+ldap_probe.exceptions
+---------------------
+
+This module contains `custom exceptions
+<https://docs.python.org/3.6/tutorial/errors.html#user-defined-exceptions>`__
+used by the modules of the :ref:`Domain Controllers Monitoring Application`.
+
+:copyright:
+
+    Copyright 2018 - 2019 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    serban.teodorescu@phsa.ca
+
+:updated:    Nov. 12, 2019
+
+"""
+
+
+class ADProbeControllerError(Exception):
+    """
+    Custom :exc:`exceptions.Exception` class raised when one creates an
+    instance of the :class:`ldap_probe.ad_probe.ADProbe` class without
+    providing a domain controller object
+    """
+
+
+class ADProbeOperationalError(Exception):
+    """
+    Custom :exc:`exceptions.Exception` class raised when an
+    instance of the :class:`ldap_probe.ad_probe.ADProbe` class throws an
+    exception that is not otherwise handled
+    """

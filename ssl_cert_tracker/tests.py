fbfda88c645bb0825f8c56ce325a9277e8d3f706
DanielBustoPHSA <57960203+DanielBustoPHSA@users.noreply.github.com>
2020-03-11 08:38:17 -0700

    First pass at ssl_cert_tracker tests

--- ssl_cert_tracker/tests.py
+++ ssl_cert_tracker/tests.py
@@ -1,1 +1,53 @@
-# Create your tests here
+"""
+ssl_cert_tracker.tests
+----------------------
+
+This module contains tests for the ssl cert tracker module.
+:copyright:
+
+    Copyright 2020 - Provincial Health Service Authority
+    of British Columbia
+
+:contact:    daniel.busto@phsa.ca
+"""
+from collections import namedtuple
+
+from django.test import TestCase
+
+from ssl_cert_tracker.models import SslCertificateIssuer, SslCertificate
+
+
+class SslCertificateIssuerTest(TestCase):
+    """
+    Tests for SSL Certificat Issuer
+    """
+    def test_getorcreate(self):
+        """
+        test that get or create returns an issuer.
+        """
+        self.assertIsInstance(SslCertificateIssuer.get_or_create({}),
+                              SslCertificateIssuer)
+
+
+class SslCertificateTest(TestCase):
+    """
+    Tests for SSL Certificate
+    """
+    def test_createorupdate(self):
+        """
+        Test that create or update does that.
+        """
+        ssl_data = namedtuple(
+            'ssl_data',
+            'ssl_issuer, port, ssl_subject, hostnames, ssl_not_before, '
+            'ssl_not_after, ssl_pem, ssl_pk_bits, ssl_pk_type, ssl_md5, '
+            'ssl_sha1')
+
+        fakes = ['fake']*4
+
+        self.assertTrue(
+            SslCertificate.create_or_update(
+                3253, ssl_data(
+                    {}, 443, {}, 'hosts', '2000-01-01 00:00',
+                    '2000-01-01 00:00', 'pem', *fakes)
+            )[0])

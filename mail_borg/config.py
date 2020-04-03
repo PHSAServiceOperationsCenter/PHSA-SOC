86f46f4d79021c4887955e6da6aa5ae6efb0094a
DanielBustoPHSA <57960203+DanielBustoPHSA@users.noreply.github.com>
2020-04-03 13:37:02 -0700

    Fixes based on pylint feedback.

--- mail_borg/config.py
+++ mail_borg/config.py
@@ -88,14 +88,24 @@ clicks on the ``Reset local config`` button
 
 
 class ConfigManager:
+    """
+    A middleman to handle the access to various different configuration sources
+    for the mail borg application
+    """
     def __init__(self):
+        """
+        Constructor for ConfigManager
+        """
         self.server_config = self._load_server_configuration()
-        self.server = ConfigServer.make_server(self.server_config)
+        self.server = ConfigServer(self.server_config)
         # TODO what to do about local config?
         self.local = False
 
     @property
     def app_config(self):
+        """
+        The configuration for the mail borg application
+        """
         if self.local:
             return None
         return self.server.config
@@ -159,17 +169,15 @@ class ConfigManager:
 
         return config
 
-    def clear_conifg(self):
+    def clear_config(self):
+        """
+        Remove all application configuration values
+        """
         self.server.clear_config()
 
     def reset_server_configuration(self):
         """
-        reload the basic configuration from the :attr:`INI_DEFAULTS`
-
-        :note:
-
-            it is possible that the values in the :attr:`INI_DEFAULTS` are out of
-            date
+        reload the basic configuration from the :attr:`INI_DEFAULTS
 
         :returns: :class:`dict` with the default basic configuration
         """
@@ -185,7 +193,6 @@ class ConfigManager:
 
             The name of the INI file containing the basic configuration.
             Default is 'mail_borg.ini'.
-
         """
         config_parser = configparser.ConfigParser(
             allow_no_value=True, empty_lines_in_values=False)
@@ -193,13 +200,22 @@ class ConfigManager:
             collections.OrderedDict(('SITE', self.server_config)),
             source='<collections.OrderedDict>')
 
-        with open(config_file, 'w') as f:
-            config_parser.write(f, space_around_delimiters=True)
+        with open(config_file, 'w') as file:
+            config_parser.write(file, space_around_delimiters=True)
 
 
 class ConfigServer:
+    """
+    Class that manages interactions with the server that stores the application
+    configuration.
+    """
     def __init__(self, srv_config):
-        self.ip = srv_config['cfg_srv_ip']
+        """
+        Constructor for config server
+
+        :param srv_config: Configuration for the server connection
+        """
+        self.ip = srv_config['cfg_srv_ip'] # pylint: disable=invalid-name
         self.port = srv_config['cfg_srv_port']
         self.connection_timeout = srv_config['cfg_srv_conn_timeout']
         self.read_timeout = srv_config['cfg_srv_read_timeout']
@@ -208,6 +224,9 @@ class ConfigServer:
 
     @property
     def config(self):
+        """
+        App configuration as stored on the server.
+        """
         if not self._config:
             self._load_config()
         return self._config
@@ -217,45 +236,11 @@ class ConfigServer:
         load the main configuration for the :ref:`Mail Borg Client Application`
 
         See :ref:`borg_remote_config` for more details about the structure of
-        the
-        main configuration.
-
-        If the value of the ``use_cfg_srv`` key in the basic configuration is
-        ``True``, the main configuration is loaded from the ``SOC Automation
-        server`` via a REST over HTTP(S) call.
-        Otherwise, the main configuration is loaded from the
-        ``mail_borg.json`` JSON file. This file must be present in the
-        same directory. Normally, this file is created the first time the
-        :ref:`Mail Borg Client Application` is requesting a remote
-        configuration.
-        It is possible (but not recommended) to use a hand-crafted version of
-        this
-        file.
-
-        When the main configuration is successfully loaded from the server, this
-        function will cache the configuration to the ``mail_borg.json`` file.
-
-        This function is also responsible for invoking the
-        :func:`parse_duration`
-        function in order to de-serialize duration fields to minutes or seconds
-        expressed as :class:`int`.
-
-        :returns:
-
-            a complex structure that results from reading the JSON encoded
-            string
-            described in :ref:`borg_client_config` into Python
-
-        :raises:
-
-            generic :exc:`Exception` errors that are caught and used to update
-            status fields in the main window of the
-            :mod:`mail_borg.mail_borg_gui`
-            module
+        the main configuration.
         """
         self._config = dict()
 
-        url = self._build_rest_url()
+        url = self._rest_url()
 
         try:
             response = SESSION.get(
@@ -279,7 +264,10 @@ class ConfigServer:
             with open(LOCAL_CONFIG, 'w') as local_config:
                 json.dump(self._config, local_config, indent=4)
 
-    def _build_rest_url(self):
+    def _rest_url(self):
+        """
+        The URL to use to retrieve the configuration
+        """
         rest_endpoint = 'mail_collector/api/get_config'
         SESSION.verify = VERIFY_SSL
         hostname = socket.gethostname()
@@ -288,10 +276,8 @@ class ConfigServer:
                f'{hostname}/'
 
     def clear_config(self):
+        """
+        Remove the config from the object.
+        Does not affect the configuration file on server.
+        """
         self._config = None
-
-    @classmethod
-    def make_server(cls, server_config=None):
-        if not server_config:
-            server_config = cls.load_server_configuration()
-        return cls(server_config)

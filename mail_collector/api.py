f16df399ec8bb5fa3e97a6de20afad162c08d137
DanielBustoPHSA <57960203+DanielBustoPHSA@users.noreply.github.com>
2020-04-07 14:49:33 -0700

    Not found is the correct response when the requested object is unrecognized. Not acceptable refers to the metadata of the request.

--- mail_collector/api.py
+++ mail_collector/api.py
@@ -37,7 +37,7 @@ def get_bot_config(request, host_name):
         the bot config JSON encoded if available, or a default configuration
         also JSON encoded when the host_name is not known to the
         server. if neither configurations are available,
-        an HTTP 406 (not acceptable) error is returned
+        an HTTP 404 (not found) error is returned
 
     :rtype: :class:`rest_framework.response.Response`
 
@@ -45,19 +45,20 @@ def get_bot_config(request, host_name):
     queryset = WinlogbeatHost.objects.filter(host_name__iexact=host_name)
 
     if not queryset.exists():
-        queryset = WinlogbeatHost.objects.filter(host_name__iexact='host.not.exist')
-        serializer = BotConfigSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(f'Bot at {host_name} is not recognized by the '
+                        f'Exchange monitoring client.',
+                        status=status.HTTP_404_NOT_FOUND)
 
     bot = queryset.get()
+
     if bot.exchange_client_config is None:
         try:
             exchange_client_config = ExchangeConfiguration.objects.filter(
                 is_default=True).get()
         except ExchangeConfiguration.DoesNotExist:
             return Response(
-                ('Bot at %s does not have a configuration for the Exchange'
-                 ' monitoring client.' % host_name),
+                (f'Bot at {host_name} does not have a configuration for the '
+                 f'Exchange monitoring client.'),
                 status=status.HTTP_404_NOT_FOUND)
 
         bot.exchange_client_config = exchange_client_config

5af961f5639f93488f654bc6bd7d75d740f77574
DanielBustoPHSA <57960203+DanielBustoPHSA@users.noreply.github.com>
2020-07-29 15:27:34 -0700

    SFTP first pass.

---
+++ sftp/apps.py
@@ -0,0 +1,29 @@
+"""
+.. _base_apps:
+
+p_soc_auto_base.apps module
+---------------------------
+
+This module contains the `Django` application configuration for sftp.
+
+
+:module:    p_soc_auto.sftp.apps
+
+:copyright:
+
+    Copyright 2020 Provincial Health Service Authority
+    of British Columbia
+
+:contact:    daniel.busto@phsa.ca
+"""
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class SftpConfig(AppConfig):
+    """
+    App class for the sftp application
+    """
+    name = 'sftp'
+    verbose_name = _('SOC SFTP Application')
